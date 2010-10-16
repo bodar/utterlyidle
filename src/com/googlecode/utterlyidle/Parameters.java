0bb12853fd8a79d0e61c13cac4551fdee5d94232
Daniel Worthington-Bodart <dan@bodar.com>
2010-10-16 16:59:03 +0100

    Minor clean up

--- src/com/googlecode/utterlyidle/Parameters.java
+++ src/com/googlecode/utterlyidle/Parameters.java
@@ -1,9 +1,11 @@
 package com.googlecode.utterlyidle;
 
 import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Callable2;
 import com.googlecode.totallylazy.Callables;
 import com.googlecode.totallylazy.Pair;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
@@ -34,6 +36,30 @@ public class Parameters implements Iterable<Pair<String, List<String>>> {
 
     public Iterator<Pair<String, List<String>>> iterator() {
         return sequence(values.entrySet()).map(Callables.<String, List<String>>entryToPair()).iterator();
+    }
+
+    public static <T extends Parameters> Callable2<T, String, T> addParameter(final String name) {
+        return new Callable2<T, String, T>() {
+            public T call(T result, String value) throws Exception {
+                return (T) result.add(name, value);
+            }
+        };
+    }
+
+    public static <T extends Parameters> Callable2<T, String, T> addParameters(final HttpServletRequest request) {
+        return new Callable2<T, String, T>() {
+            public T call(T result, final String name) throws Exception {
+                return (T) sequence(request.getParameterValues(name)).foldLeft(result, addParameter(name));
+            }
+        };
+    }
+
+    public static Callable2<Parameters, Pair<String, String>, Parameters> pairIntoParameters() {
+        return new Callable2<Parameters, Pair<String, String>, Parameters>() {
+            public Parameters call(Parameters result, Pair<String, String> pair) throws Exception {
+                return result.add(pair.first(), pair.second());
+            }
+        };
     }
 
 }
