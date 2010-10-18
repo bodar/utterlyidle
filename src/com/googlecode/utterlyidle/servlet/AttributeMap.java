3638f3d48e5a5503e0e0ee2f778c55562da40d67
Daniel Worthington-Bodart <dan@bodar.com>
2010-10-18 13:25:14 +0100

    Changed to not copy values across from ServletContext

--- src/com/googlecode/utterlyidle/servlet/AttributeMap.java
+++ src/com/googlecode/utterlyidle/servlet/AttributeMap.java
@@ -1,24 +1,26 @@
 package com.googlecode.utterlyidle.servlet;
 
-import com.googlecode.totallylazy.Callable2;
-import com.googlecode.totallylazy.Sequences;
-
 import javax.servlet.ServletContext;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.googlecode.totallylazy.Sequences.sequence;
-
 public class AttributeMap {
-    private final Map<String, Object> values = new HashMap<String, Object>();
-
     public static AttributeMap attributeMap(final ServletContext context){
-        return sequence(context.getAttributeNames(), String.class).foldLeft(attributeMap(), new Callable2<AttributeMap, String, AttributeMap>() {
-            public AttributeMap call(AttributeMap attributes, String name) throws Exception {
-                return attributes.put(name, context.getAttribute(name));
+        return new AttributeMap() {
+            @Override
+            public AttributeMap put(String name, Object value) {
+                context.setAttribute(name, value);
+                return this;
             }
-        });
+
+            @Override
+            <T> T get(String name, Class<T> aClass) {
+                return aClass.cast(context.getAttribute(name));
+            }
+        };
     }
+
+    private final Map<String, Object> values = new HashMap<String, Object>();
 
     public AttributeMap put(String name, Object value) {
         values.put(name, value);
