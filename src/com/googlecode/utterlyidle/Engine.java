9ce4766f479f99437812c2f7d2a80b9e2ecb05a1
Daniel Worthington-Bodart <dan@bodar.com>
2010-10-19 16:26:29 +0100

    Added renderer to Engine interface

--- src/com/googlecode/utterlyidle/Engine.java
+++ src/com/googlecode/utterlyidle/Engine.java
@@ -5,4 +5,6 @@ import com.googlecode.yadic.Resolver;
 public interface Engine {
     void add(Class resource);
     void handle(Resolver resolver, Request request, Response response);
+
+    <T> void addRenderer(Class<T> customClass, Renderer<T> renderer);
 }
