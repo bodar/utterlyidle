1b595fb341924fefa8c37683533c47909626ed6f
Daniel Worthington-Bodart <dan@bodar.com>
2010-10-21 11:59:41 +0100

    Added support for custom exception rendering

--- src/com/googlecode/utterlyidle/CoreModule.java
+++ src/com/googlecode/utterlyidle/CoreModule.java
@@ -1,6 +1,18 @@
 package com.googlecode.utterlyidle;
 
+import com.googlecode.utterlyidle.handlers.ExceptionHandler;
+import com.googlecode.utterlyidle.handlers.NullHandler;
+import com.googlecode.utterlyidle.handlers.RedirectHandler;
+import com.googlecode.utterlyidle.handlers.RendererHandler;
+import com.googlecode.utterlyidle.handlers.ResponseHandlers;
+import com.googlecode.utterlyidle.handlers.StreamingOutputHandler;
+import com.googlecode.utterlyidle.handlers.StreamingWriterHandler;
 import com.googlecode.yadic.Container;
+
+import javax.ws.rs.core.StreamingOutput;
+
+import static com.googlecode.totallylazy.Predicates.aNull;
+import static com.googlecode.totallylazy.Predicates.assignableTo;
 
 
 public class CoreModule implements Module{
@@ -10,11 +22,22 @@ public class CoreModule implements Module{
     }
 
     public Module addPerApplicationObjects(Container container) {
-        container.add(RestEngine.class);
+        container.add(Engine.class, RestEngine.class);
         return this;
     }
 
     public Module addResources(Engine engine) {
+        final ResponseHandlers handlers = engine.responseHandlers();
+        final RendererHandler renderers = engine.renderers();
+
+        handlers.addGuard(aNull(Object.class), NullHandler.class);
+        handlers.addGuard(assignableTo(Redirect.class), RedirectHandler.class);
+        handlers.addGuard(assignableTo(StreamingWriter.class), StreamingWriterHandler.class);
+        handlers.addGuard(assignableTo(StreamingOutput.class), StreamingOutputHandler.class);
+        handlers.addCatchAll(assignableTo(UnsupportedOperationException.class), new ExceptionHandler(Status.NOT_IMPLEMENTED, renderers));
+        handlers.addCatchAll(assignableTo(Exception.class), new ExceptionHandler(Status.INTERNAL_SERVER_ERROR, renderers));
+        handlers.addCatchAll(assignableTo(Object.class), renderers);
+
         return this;
     }
 }
--- src/com/googlecode/utterlyidle/handlers/NullHandler.java
+++ src/com/googlecode/utterlyidle/handlers/NullHandler.java
@@ -3,11 +3,12 @@ package com.googlecode.utterlyidle.handlers;
 import com.googlecode.utterlyidle.Response;
 import com.googlecode.utterlyidle.ResponseHandler;
 import com.googlecode.utterlyidle.Status;
+import com.googlecode.yadic.Resolver;
 
 import java.io.IOException;
 
 public class NullHandler implements ResponseHandler<Object> {
-    public void handle(Object value, Response response) throws IOException {
+    public void handle(Object value, Resolver resolver, Response response) throws IOException {
         response.code(Status.NO_CONTENT);
     }
 }
