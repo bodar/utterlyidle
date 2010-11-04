0a358ee0ea9661eebb6a43f09274a98e91ff96df
Daniel Wothington-Bodart <dan@bodar.com>
2010-11-04 13:38:20 +0000

    Removed convenience callable

--- src/com/googlecode/utterlyidle/rendering/Models.java
+++ src/com/googlecode/utterlyidle/rendering/Models.java
@@ -6,16 +6,6 @@ import com.googlecode.totallylazy.Option;
 import static com.googlecode.utterlyidle.rendering.Model.model;
 
 public class Models {
-    public static Callable2<Model, String, Model> handleInvalidValue(final String fieldName, final String message) {
-        return new Callable2<Model, String, Model>() {
-            public Model call(Model model, String invalidValue) throws Exception {
-                return model.add("field", model().
-                        add("name", fieldName).
-                        add("value", invalidValue).
-                        add("message", message));
-            }
-        };
-    }
 
     public static <T> Callable2<Model, Option<T>, Model> handleOptional(final Callable2<Model, T, Model> callable) {
         return new Callable2<Model, Option<T>, Model>() {
