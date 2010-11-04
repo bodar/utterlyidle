7ec94928382b664e0c42999050a2301e9f3ec8b4
Daniel Worthington-Bodart <dan@bodar.com>
2010-11-04 11:37:21 +0000

    Added crazy support for Either combined with Option

--- test/com/googlecode/utterlyidle/SomeResource.java
+++ test/com/googlecode/utterlyidle/SomeResource.java
@@ -1,5 +1,7 @@
 package com.googlecode.utterlyidle;
 
+import com.googlecode.totallylazy.Option;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,6 +17,11 @@ public class SomeResource {
 
     @GET
     public String getHtml(@PathParam("id") String id) {
+        return "bob";
+    }
+
+    @GET
+    public String getHtml(@PathParam("id") Option<String> someValue) {
         return "bob";
     }
 
