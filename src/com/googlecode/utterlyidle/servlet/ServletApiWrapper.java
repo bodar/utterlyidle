7bc6735924258aee09855304882c22226b2ab622
Daniel Worthington-Bodart <dan@bodar.com>
2010-11-10 18:19:00 +0000

    Minor refactor

--- src/com/googlecode/utterlyidle/servlet/ServletApiWrapper.java
+++ src/com/googlecode/utterlyidle/servlet/ServletApiWrapper.java
@@ -1,16 +1,10 @@
 package com.googlecode.utterlyidle.servlet;
 
 import com.googlecode.utterlyidle.AttributeMap;
-import com.googlecode.utterlyidle.Request;
-import com.googlecode.utterlyidle.Response;
-import com.googlecode.utterlyidle.Status;
 import com.googlecode.utterlyidle.WebRoot;
 import com.googlecode.utterlyidle.io.Url;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.Callable;
@@ -18,13 +12,6 @@ import java.util.concurrent.Callable;
 import static com.googlecode.utterlyidle.io.Url.url;
 
 public class ServletApiWrapper {
-    public static Request request(HttpServletRequest request) {
-        try {
-            return new ServletRequest(request);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static Callable<WebRoot> webRoot(final ServletContext context) {
         return new Callable<WebRoot>() {
@@ -59,25 +46,4 @@ public class ServletApiWrapper {
         };
     }
 
-    public static Response response(final HttpServletResponse response) {
-        try {
-            return new Response(response.getOutputStream()) {
-                @Override
-                public Response header(String name, String value) {
-                    response.setHeader(name, value);
-                    super.header(name, value);
-                    return this;
-                }
-
-                @Override
-                public Response code(Status value) {
-                    response.setStatus(value.code());
-                    super.code(value);
-                    return this;
-                }
-            };
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
