8946d307b1aaf974f5b1e1d87df3d60d200e72c8
Matthew Savage <savagematt@users.noreply.github.com>
2010-11-12 15:01:51 +0000

    Pedantic renaming to RFC 2616

--- src/com/googlecode/utterlyidle/Rfc2616.java
+++ src/com/googlecode/utterlyidle/Rfc2616.java
@@ -8,8 +8,8 @@ import static com.googlecode.totallylazy.Sequences.range;
 import static java.util.Arrays.asList;
 
 public class Rfc2616 {
-    public static final String ILLEGAL_CHARACTERS = "()<>@,;:\\\"/[]?={} \t";
-    public static final String ASCII_CONTROL_CHARACTERS = range(0,32).join(asList(127)).fold("", charactersToString());
+    public static final String SEPARATORS = "()<>@,;:\\\"/[]?={} \t";
+    public static final String CTLs = range(0,32).join(asList(127)).fold("", charactersToString());
 
     public static boolean isValidToken(String value) {
         if(value == null || value.length()==0)return false;
@@ -19,7 +19,7 @@ public class Rfc2616 {
     public static Predicate<? super Character> isValidTokenCharacter() {
         return new Predicate<Character>() {
             public boolean matches(Character character) {
-                return ILLEGAL_CHARACTERS.indexOf(character) < 0 && ASCII_CONTROL_CHARACTERS.indexOf(character) < 0;
+                return SEPARATORS.indexOf(character) < 0 && CTLs.indexOf(character) < 0;
             }
         };
     }
