16a523ede7e6f680779f59aba0413d83a11f7fa5
Daniel Worthington-Bodart <dan@bodar.com>
2010-12-08 11:49:44 +0000

    Added list of decorators to SiteMeshHandler

--- src/com/googlecode/utterlyidle/sitemesh/SiteMeshHandler.java
+++ src/com/googlecode/utterlyidle/sitemesh/SiteMeshHandler.java
@@ -1,27 +1,59 @@
 package com.googlecode.utterlyidle.sitemesh;
 
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Callable2;
+import com.googlecode.totallylazy.Callables;
+import com.googlecode.totallylazy.Option;
+import com.googlecode.totallylazy.Predicate;
+import com.googlecode.totallylazy.Predicates;
+import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.Sequences;
 import com.googlecode.utterlyidle.BasePath;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.RequestHandler;
 import com.googlecode.utterlyidle.Response;
 import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
+
+import static com.googlecode.totallylazy.Pair.pair;
+import static com.googlecode.totallylazy.Predicates.where;
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.utterlyidle.sitemesh.DecoratorRule.asTemplateName;
+import static com.googlecode.utterlyidle.sitemesh.DecoratorRule.matchingRule;
 
 public class SiteMeshHandler implements RequestHandler {
     private final RequestHandler requestHandler;
     private final BasePath base;
     private final Includer include;
+    private final Sequence<DecoratorRule> decorators;
+    private final StringTemplateGroup group;
 
-    public SiteMeshHandler(final RequestHandler requestHandler, final BasePath base, final Includer include) {
+    public SiteMeshHandler(final RequestHandler requestHandler, final BasePath base, final Includer include, final Iterable<DecoratorRule> decorators, final StringTemplateGroup templateGroup) {
         this.requestHandler = requestHandler;
         this.base = base;
         this.include = include;
+        this.decorators = sequence(decorators);
+        group = templateGroup;
     }
 
     public void handle(final Request request, final Response response) throws Exception {
-        StringTemplate template = new StringTemplate("$body$ World!");
-        StringTemplateDecorator decorator = new StringTemplateDecorator(template, include, base,request.query());
-        Response decoratetResponse = response.output(new SiteMeshOutputStream(response.output(), decorator));
-        requestHandler.handle(request, decoratetResponse);
+        Option<TemplateName> templateName = getTemplateNameFor(request, response);
+        Response decoratedResponse = templateName.fold(response, decorateOutputStream(request));
+        requestHandler.handle(request, decoratedResponse);
     }
 
+    private Callable2<Response, TemplateName, Response> decorateOutputStream(final Request request) {
+        return new Callable2<Response, TemplateName, Response>() {
+            public Response call(Response response, TemplateName templateName) throws Exception {
+                StringTemplate template = group.getInstanceOf(templateName.name());
+                StringTemplateDecorator decorator = new StringTemplateDecorator(template, include, base, request.query());
+                return response.output(new SiteMeshOutputStream(response.output(), decorator));
+
+            }
+        };
+    }
+
+    private Option<TemplateName> getTemplateNameFor(final Request request, final Response response) {
+        return decorators.find(matchingRule(request, response)).map(asTemplateName());
+    }
 }
--- test/com/googlecode/utterlyidle/sitemesh/SiteMeshHandlerTest.java
+++ test/com/googlecode/utterlyidle/sitemesh/SiteMeshHandlerTest.java
@@ -1,29 +1,44 @@
 package com.googlecode.utterlyidle.sitemesh;
 
 import com.googlecode.totallylazy.Pair;
-import com.googlecode.totallylazy.Predicate;
+import com.googlecode.totallylazy.Predicates;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.RequestHandler;
 import com.googlecode.utterlyidle.Response;
+import org.antlr.stringtemplate.StringTemplateGroup;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.util.List;
 
-import static com.googlecode.totallylazy.Predicates.not;
 import static com.googlecode.utterlyidle.RequestBuilder.get;
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 public class SiteMeshHandlerTest {
     @Test
+    public void shouldNotDecorateHtmlWhenNoDecorators() throws Exception{
+        OutputStream outputStream = new ByteArrayOutputStream();
+        Response response = Response.response(outputStream);
+        RequestHandler hello = write("<body>Hello</body>");
+        List<DecoratorRule> decorators = emptyList();
+        RequestHandler decorator = new SiteMeshHandler(hello, null, new Includer(), decorators, getWorldGroup());
+
+        decorator.handle(get(null).build(), response);
+
+        assertThat(outputStream.toString(), containsString("<body>Hello</body>"));
+    }
+
+    @Test
     public void shouldDecorateHtml() throws Exception{
         OutputStream outputStream = new ByteArrayOutputStream();
         Response response = Response.response(outputStream);
         RequestHandler hello = write("<body>Hello</body>");
-        RequestHandler decorator = new SiteMeshHandler(hello, null, new Includer());
+        List<DecoratorRule> decorators = asList(new DecoratorRule(Predicates.<Pair<Request, Response>>always(), new TemplateName("world")));
+        RequestHandler decorator = new SiteMeshHandler(hello, null, new Includer(), decorators, getWorldGroup());
 
         decorator.handle(get(null).build(), response);
 
@@ -36,26 +51,21 @@ public class SiteMeshHandlerTest {
         Response response = Response.response(outputStream);
         RequestHandler hello = write("<body>Hello</body>");
 
-        List<Predicate<Pair<Request, Response>>> decorators = asList(none(), all());
+        List<DecoratorRule> decorators = asList(new DecoratorRule(Predicates.<Pair<Request, Response>>never(), new TemplateName("shouldNeverSeeMe!")), new DecoratorRule(Predicates.<Pair<Request, Response>>always(), new TemplateName("world")));
 
-        RequestHandler decorator = new SiteMeshHandler(hello, null, new Includer());
+        RequestHandler decorator = new SiteMeshHandler(hello, null, new Includer(), decorators, getWorldGroup());
 
         decorator.handle(get(null).build(), response);
 
         assertThat(outputStream.toString(), containsString("Hello World!"));
     }
 
-    private Predicate<Pair<Request, Response>> all() {
-        return not(none());
+    private StringTemplateGroup getWorldGroup() {
+        StringTemplateGroup groups = new StringTemplateGroup("decorators");
+        groups.defineTemplate("world", "$body$ World!");
+        return groups;
     }
 
-    private Predicate<Pair<Request, Response>> none() {
-        return new Predicate<Pair<Request, Response>>() {
-            public boolean matches(Pair<Request, Response> requestResponsePair) {
-                return false;
-            }
-        };
-    }
 
     private RequestHandler write(final String value) {
         return new RequestHandler() {
