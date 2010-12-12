c187b2b24aed76e0b715f95d4b5aecfe0cd1a73f
Matthew Savage <savagematt@users.noreply.github.com>
2010-12-12 19:33:58 +0000

    ParametersExtractor uses Parameters object if it is present in one of the method arguments

--- src/com/googlecode/utterlyidle/ParametersExtractor.java
+++ src/com/googlecode/utterlyidle/ParametersExtractor.java
@@ -1,17 +1,12 @@
 package com.googlecode.utterlyidle;
 
-import com.googlecode.totallylazy.Callable2;
-import com.googlecode.totallylazy.Left;
-import com.googlecode.totallylazy.None;
-import com.googlecode.totallylazy.Pair;
-import com.googlecode.totallylazy.Right;
-import com.googlecode.totallylazy.Sequence;
-import com.googlecode.totallylazy.Some;
+import com.googlecode.totallylazy.*;
 
-import javax.swing.text.html.Option;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.BitSet;
 
+import static com.googlecode.totallylazy.Callables.first;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.utterlyidle.Param.isParam;
 import static com.googlecode.utterlyidle.Param.toParam;
@@ -24,12 +19,20 @@ public class ParametersExtractor {
     }
 
     public <T extends Parameters> T extract(T parameters, Class<? extends Annotation> aClass){
+        if(!argumentOfType(parameters.getClass()).isEmpty()){
+            return (T) argumentOfType(parameters.getClass()).get();
+        }
+
         for (Pair<Object, Annotation[]> pair : pairs) {
             final Object value = pair.first();
             final Sequence<Annotation> annotations = sequence(pair.second()).filter(isParam());
             annotations.safeCast(aClass).map(toParam()).foldLeft(parameters, add(value));
         }
         return parameters;
+    }
+
+    private <T> Option<T> argumentOfType(Class<T> aClass) {
+        return pairs.map(first()).safeCast(aClass).headOption();
     }
 
     public static Callable2<Parameters, Param, Parameters> add(final Object value) {
