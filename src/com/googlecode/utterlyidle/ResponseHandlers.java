6aac4221682581e5808672eafb20f8de7ed4ad76
Daniel Worthington-Bodart <dan@bodar.com>
2010-12-22 03:52:11 +0000

    Renderers/Handlers can now match against the request and response

--- src/com/googlecode/utterlyidle/ResponseHandlers.java
+++ src/com/googlecode/utterlyidle/ResponseHandlers.java
@@ -1,14 +1,11 @@
 package com.googlecode.utterlyidle;
 
-import com.googlecode.totallylazy.Callable1;
 import com.googlecode.utterlyidle.handlers.ResponseHandlerRegistry;
 import com.googlecode.yadic.Resolver;
 
-import static com.googlecode.totallylazy.Callables.first;
-import static com.googlecode.totallylazy.Callables.second;
 import static com.googlecode.totallylazy.Callers.call;
-import static com.googlecode.totallylazy.Predicates.by;
-import static com.googlecode.totallylazy.Predicates.matches;
+import static com.googlecode.utterlyidle.handlers.HandlerRule.handler;
+import static com.googlecode.utterlyidle.handlers.HandlerRule.matches;
 import static com.googlecode.yadic.CreateCallable.create;
 
 public class ResponseHandlers implements ResponseHandler{
@@ -20,9 +17,8 @@ public class ResponseHandlers implements ResponseHandler{
         this.resolver = resolver;
     }
 
-    @SuppressWarnings("unchecked")
-    private ResponseHandler findHandler(Request request, Response response){
-        final Object handler = registry.handlers().filter(by((Callable1) first(), matches(response.entity()))).map(second()).head();
+    private ResponseHandler findHandler(final Request request, final Response response){
+        final Object handler = registry.handlers().filter(matches(request, response)).map(handler()).head();
         if (handler instanceof Class) {
             return (ResponseHandler) call(create((Class) handler, resolver));
         }
---
+++ src/com/googlecode/utterlyidle/handlers/HandlerRule.java
@@ -0,0 +1,48 @@
+package com.googlecode.utterlyidle.handlers;
+
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.Predicate;
+import com.googlecode.utterlyidle.Request;
+import com.googlecode.utterlyidle.Response;
+
+import static com.googlecode.totallylazy.Pair.pair;
+
+public class HandlerRule {
+    private final Predicate<? super Pair<Request, Response>> predicate;
+    private final Object handler;
+
+    private HandlerRule(Predicate<? super Pair<Request, Response>> predicate, Object handler) {
+        this.predicate = predicate;
+        this.handler = handler;
+    }
+
+    public static HandlerRule rule(Predicate<? super Pair<Request, Response>> predicate, Object handler) {
+        return new HandlerRule(predicate, handler);
+    }
+
+    public static Predicate<HandlerRule> matches(final Request request, final Response response) {
+        return new Predicate<HandlerRule>() {
+            public boolean matches(HandlerRule handlerRule) {
+                return handlerRule.predicate.matches(pair(request, response));
+            }
+        };
+    }
+
+    public static Callable1<? super HandlerRule, Object> handler() {
+        return new Callable1<HandlerRule, Object>() {
+            public Object call(HandlerRule handlerRule) throws Exception {
+                return handlerRule.handler;
+            }
+        };
+    }
+
+    public static Callable1<? super Pair<Request, Response>, Object> entity() {
+        return new Callable1<Pair<Request, Response>, Object>() {
+            public Object call(Pair<Request, Response> pair) throws Exception {
+                return pair.second().entity();
+            }
+        };
+    }
+
+}
