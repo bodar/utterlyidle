4457dca95c04b8094114c8a3211ba5a58ba4a651
Daniel Worthington-Bodart <dan@bodar.com>
2011-01-07 17:36:20 +0000

    Checkin so can carry on at home

--- src/com/googlecode/utterlyidle/StaticMethodResolver.java
+++ src/com/googlecode/utterlyidle/StaticMethodResolver.java
@@ -7,32 +7,33 @@ import com.googlecode.yadic.Resolver;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.concurrent.Callable;
 
 import static com.googlecode.totallylazy.Option.none;
 import static com.googlecode.totallylazy.Option.some;
 import static com.googlecode.totallylazy.Predicates.*;
 import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.yadic.generics.Types.classOf;
 import static java.lang.reflect.Modifier.PUBLIC;
 import static java.lang.reflect.Modifier.STATIC;
 
-public class StaticMethodActivator<T> implements Callable<T> {
-    private final Class<T> returnType;
+public class StaticMethodResolver<T> implements Resolver<T> {
     private final Resolver resolver;
     private final Class<?> argumentType;
 
-    public StaticMethodActivator(Class<T> returnType, Resolver resolver, Class<?> argumentType) {
-        this.returnType = returnType;
+    public StaticMethodResolver(Resolver resolver, Class<?> argumentType) {
         this.resolver = resolver;
         this.argumentType = argumentType;
     }
 
-    public T call() throws Exception {
-        final Sequence<Method> methods = sequence(returnType.getMethods()).filter(and(modifier(PUBLIC), modifier(STATIC), arguments(argumentType)));
+    public T resolve(Type type) throws Exception {
+        final Class returnType = classOf(type);
+        final Sequence<Method> methods = sequence(returnType.getMethods()).filter(modifier(PUBLIC).and(modifier(STATIC)).and(arguments(argumentType)));
         return methods.pick(new Callable1<Method, Option<T>>() {
             public Option<T> call(Method method) throws Exception {
                 try {
-                    return some(returnType.cast(method.invoke(null, resolver.resolve(argumentType))));
+                    return (Option<T>) some(returnType.cast(method.invoke(null, resolver.resolve(argumentType))));
                 } catch (InvocationTargetException e) {
                     return none();
                 }
