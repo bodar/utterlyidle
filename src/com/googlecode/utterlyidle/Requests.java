70a7495af4db80e1c7a6627ee138914b9557ad9f
Daniel Wothington-Bodart <dan@bodar.com>
2011-01-31 17:28:17 +0000

    Using totallylazy to convert streams to byte[]

--- src/com/googlecode/utterlyidle/Requests.java
+++ src/com/googlecode/utterlyidle/Requests.java
@@ -20,19 +20,4 @@ public class Requests {
     public static Request request(String method, String path, QueryParameters query, HeaderParameters headers, byte[] input) {
         return request(method, Url.url(path + query.toString()), headers, input);
     }
-
-    public static byte[] getBytes(InputStream stream) {
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            byte[] buffer = new byte[512];
-            int read = stream.read(buffer);
-            while (read > 0) {
-                outputStream.write(buffer, 0, read);
-                read = stream.read(buffer);
-            }
-            return outputStream.toByteArray();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
