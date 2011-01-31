d88c93879a5454d582d4dc78fd4474ce3ec4e02d
Daniel Wothington-Bodart <dan@bodar.com>
2011-01-31 12:02:24 +0000

    Removed inheritance from MemoryRequest

--- src/com/googlecode/utterlyidle/servlet/ApplicationServlet.java
+++ src/com/googlecode/utterlyidle/servlet/ApplicationServlet.java
@@ -1,6 +1,7 @@
 package com.googlecode.utterlyidle.servlet;
 
-import com.googlecode.utterlyidle.Application;
+import com.googlecode.utterlyidle.*;
+import com.googlecode.utterlyidle.io.Url;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -8,7 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static com.googlecode.utterlyidle.servlet.ServletRequest.request;
+import java.io.IOException;
+
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.utterlyidle.Requests.getBytes;
+import static com.googlecode.utterlyidle.io.Url.url;
 import static com.googlecode.utterlyidle.servlet.ServletResponse.response;
 
 public class ApplicationServlet extends HttpServlet {
@@ -27,4 +32,36 @@ public class ApplicationServlet extends HttpServlet {
             throw new ServletException(e);
         }
     }
+
+    private Request request(HttpServletRequest request) {
+        try {
+            return Requests.request(
+                    request.getMethod(),
+                    url(request.getRequestURI() + queryString(request.getQueryString())),
+                    convertToHeaderParameters(request),
+                    getBytes(request.getInputStream()),
+                    extractBasePath(request));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String queryString(String value) {
+        return (value != null && value.length() > 0) ? "?" + value : "";
+    }
+
+    private static HeaderParameters convertToHeaderParameters(HttpServletRequest request) {
+        HeaderParameters result = HeaderParameters.headerParameters();
+        for (String name : sequence(request.getHeaderNames(), String.class)) {
+            for (String value : sequence(request.getHeaders(name), String.class)) {
+                result.add(name, value);
+            }
+        }
+        return result;
+    }
+
+    private static BasePath extractBasePath(HttpServletRequest request) {
+        return BasePath.basePath(request.getContextPath() + request.getServletPath());
+    }
+
 }
