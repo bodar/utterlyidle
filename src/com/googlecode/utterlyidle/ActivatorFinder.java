42bce4053102fd002a13b12f304b88ad876834d4
Daniel Worthington-Bodart <dan@bodar.com>
2011-02-02 17:54:11 +0000

    Moved more behavour out of RestEngine, now it's just a Annotation based list of activators

--- src/com/googlecode/utterlyidle/ActivatorFinder.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.utterlyidle;
-
-import com.googlecode.totallylazy.Either;
-
-public interface ActivatorFinder {
-    Either<MatchFailure, Activator> findActivator(Request request);
-}
