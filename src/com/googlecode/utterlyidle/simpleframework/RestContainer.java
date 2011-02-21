c78be006a1b1fe6c9c6c4da5196db073fd87c2a3
Matthew Savage <savagematt@users.noreply.github.com>
2011-02-21 22:00:05 +0000

    Implemented POST on url

---
+++ src/com/googlecode/utterlyidle/simpleframework/RestContainer.java
@@ -0,0 +1,82 @@
+package com.googlecode.utterlyidle.simpleframework;
+
+import static com.googlecode.totallylazy.Bytes.bytes;
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Callable2;
+import static com.googlecode.totallylazy.Maps.entryToPair;
+import com.googlecode.totallylazy.Pair;
+import static com.googlecode.totallylazy.Pair.pair;
+import static com.googlecode.totallylazy.Sequences.sequence;
+import com.googlecode.utterlyidle.Application;
+import com.googlecode.utterlyidle.HeaderParameters;
+import static com.googlecode.utterlyidle.HeaderParameters.headerParameters;
+import com.googlecode.utterlyidle.QueryParameters;
+import static com.googlecode.utterlyidle.QueryParameters.queryParameters;
+import com.googlecode.utterlyidle.Requests;
+import static com.googlecode.utterlyidle.io.Url.url;
+import org.simpleframework.http.Request;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.core.Container;
+
+import java.io.IOException;
+
+public class RestContainer implements Container {
+    private final Application applcation;
+
+    public RestContainer(Application applcation) {
+        this.applcation = applcation;
+    }
+
+    public void handle(Request request, Response response) {
+        try {
+            com.googlecode.utterlyidle.Response applicationResponse = applcation.handle(request(request));
+            mapTo(applicationResponse, response);
+
+            response.getPrintStream().close();
+            response.commit();
+            response.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void mapTo(com.googlecode.utterlyidle.Response applicationResponse, Response response) throws IOException {
+        response.setCode(applicationResponse.status().code());
+        sequence(applicationResponse.headers()).fold(response, mapHeaders());
+        response.getOutputStream().write(applicationResponse.bytes());
+    }
+
+    private Callable2<Response, Pair<String, String>, Response> mapHeaders() {
+        return new Callable2<Response, Pair<String, String>, Response>() {
+            public Response call(Response response, Pair<String, String> applicationHeader) throws Exception {
+                response.set(applicationHeader.first(), applicationHeader.second());
+                return response;
+            }
+        };
+    }
+
+    private com.googlecode.utterlyidle.Request request(Request request) throws IOException {
+        return Requests.request(
+                request.getMethod(),
+                request.getPath().toString(),
+                query(request),
+                headers(request),
+                bytes(request.getInputStream()));
+    }
+
+    private HeaderParameters headers(final Request request) {
+        return headerParameters(sequence(request.getNames()).map(headerValue(request)));
+    }
+
+    private Callable1<String, Pair<String, String>> headerValue(final Request request) {
+        return new Callable1<String, Pair<String, String>>() {
+            public Pair<String, String> call(String headerName) throws Exception {
+                return pair(headerName, request.getValue(headerName));
+            }
+        };
+    }
+
+    private QueryParameters query(Request request) {
+        return QueryParameters.parse(url(request.getTarget()).getQuery());
+    }
+}
