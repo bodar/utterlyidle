4e2e7e77ae85c64fbb15dcc94c55cdf8b1c3d740
Daniel Worthington-Bodart <dan@bodar.com>
2011-02-28 15:40:42 +0000

    Made UrlStringTemplateGroup close reader

--- src/com/googlecode/utterlyidle/handlers/UrlStringTemplateGroup.java
+++ src/com/googlecode/utterlyidle/handlers/UrlStringTemplateGroup.java
@@ -5,19 +5,22 @@ import org.antlr.stringtemplate.StringTemplate;
 import org.antlr.stringtemplate.StringTemplateGroup;
 
 import java.io.BufferedReader;
+import java.io.Reader;
 
 import static com.googlecode.utterlyidle.io.Url.url;
 
 public class UrlStringTemplateGroup extends StringTemplateGroup {
     public UrlStringTemplateGroup(Url baseUrl) {
         super(baseUrl.toString(), baseUrl.toString());
-        setRefreshInterval(0);
     }
 
     @Override
     protected StringTemplate loadTemplate(String name, String fileName) {
         try {
-            return loadTemplate(name, new BufferedReader(url(fileName).reader()));
+            Reader reader = url(fileName).reader();
+            StringTemplate template = loadTemplate(name, new BufferedReader(reader));
+            reader.close();
+            return template;
         } catch (Exception e) {
             return null;
         }
