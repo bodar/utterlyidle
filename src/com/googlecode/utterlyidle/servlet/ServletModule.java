49c967bc6e6a48f333012b908cd0da0221c43a91
Daniel Worthington-Bodart <dan@bodar.com>
2011-03-04 17:50:07 +0000

    Moved basePath into ThreadLocal storage for Servlet Containers! yuck

--- src/com/googlecode/utterlyidle/servlet/ServletModule.java
+++ src/com/googlecode/utterlyidle/servlet/ServletModule.java
@@ -1,9 +1,11 @@
 package com.googlecode.utterlyidle.servlet;
 
 import com.googlecode.utterlyidle.AttributeMap;
+import com.googlecode.utterlyidle.BasePath;
 import com.googlecode.utterlyidle.WebRoot;
 import com.googlecode.utterlyidle.modules.ApplicationScopedModule;
 import com.googlecode.utterlyidle.modules.Module;
+import com.googlecode.utterlyidle.modules.RequestScopedModule;
 import com.googlecode.yadic.Container;
 
 import javax.servlet.ServletContext;
@@ -11,7 +13,8 @@ import javax.servlet.ServletContext;
 import static com.googlecode.utterlyidle.servlet.ServletApiWrapper.attributeMap;
 import static com.googlecode.utterlyidle.servlet.ServletApiWrapper.webRoot;
 
-public class ServletModule implements ApplicationScopedModule {
+public class ServletModule implements ApplicationScopedModule, RequestScopedModule {
+    private final ThreadLocal<BasePath> basePath = new ThreadLocal<BasePath>();
     private final ServletContext context;
 
     public ServletModule(ServletContext context) {
@@ -21,6 +24,11 @@ public class ServletModule implements ApplicationScopedModule {
     public Module addPerApplicationObjects(Container container) {
         container.addActivator(WebRoot.class, webRoot(context));
         container.addActivator(AttributeMap.class, attributeMap(context));
+        return this;
+    }
+
+    public Module addPerRequestObjects(Container container) {
+        container.addInstance(BasePath.class, basePath.get());
         return this;
     }
 }
