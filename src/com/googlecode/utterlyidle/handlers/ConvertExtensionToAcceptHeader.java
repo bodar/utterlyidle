891e360ec59e27777fd4dda0b28ef251773763c8
Matthew Savage <savagematt@users.noreply.github.com>
2011-04-08 13:42:39 +1000

    ConvertExtensionToAcceptHeader now adds extension back in to location header on redirect

--- src/com/googlecode/utterlyidle/handlers/ConvertExtensionToAcceptHeader.java
+++ src/com/googlecode/utterlyidle/handlers/ConvertExtensionToAcceptHeader.java
@@ -1,8 +1,6 @@
 package com.googlecode.utterlyidle.handlers;
 
-import com.googlecode.totallylazy.Callable2;
-import com.googlecode.totallylazy.Pair;
-import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.*;
 import com.googlecode.totallylazy.regex.Matches;
 import com.googlecode.totallylazy.regex.Regex;
 import com.googlecode.utterlyidle.HttpHandler;
@@ -14,9 +12,11 @@ import javax.ws.rs.core.HttpHeaders;
 import java.util.Iterator;
 import java.util.regex.MatchResult;
 
+import static com.googlecode.totallylazy.Callables.returnArgument;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.regex.Regex.regex;
 import static com.googlecode.utterlyidle.io.Url.url;
+import static javax.ws.rs.core.HttpHeaders.LOCATION;
 
 public class ConvertExtensionToAcceptHeader implements HttpHandler {
     public static final Regex FILE_EXTENSION = regex("(\\.[^?/.]+?)(?:\\?.*)?$");
@@ -29,18 +29,51 @@ public class ConvertExtensionToAcceptHeader implements HttpHandler {
     }
 
     public Response handle(Request request) throws Exception {
-        Matches fileExtension = FILE_EXTENSION.findMatches(request.url().toString());
-        return decorated.handle(replacements.fold(request, applyReplacement(fileExtension)));
+        Sequence<Pair<String, String>> replacementsThatApply = replacements.filter(appliesTo(request)).realise();
+        Response response = decorated.handle(replacementsThatApply.fold(request, applyReplacement()));
+        return replacementsThatApply.fold(response, reapplyExtension());
     }
 
-    private Callable2<? super Request, ? super Pair<String, String>, Request> applyReplacement(final Matches fileExtension) {
+    private Callable2<? super Response, ? super Pair<String, String>, Response> reapplyExtension() {
+        return new Callable2<Response, Pair<String, String>, Response>() {
+            public Response call(Response response, Pair<String, String> extensionAndReplacementMimeType) throws Exception {
+                if (response.headers().contains(LOCATION)) {
+                    String originalHeader = response.headers().getValue(LOCATION);
+                    response.headers().remove(LOCATION);
+                    response.header(LOCATION, addExtension(originalHeader, extensionAndReplacementMimeType.first()));
+                }
+                return response;
+            }
+
+            private String addExtension(String url, final String extension) {
+                int queryStringStart = url.indexOf("?");
+                queryStringStart = queryStringStart >= 0 ? queryStringStart : url.length();
+                return url.substring(0, queryStringStart) + "." + extension + url.substring(queryStringStart);
+            }
+        };
+    }
+
+    private Predicate<? super Pair<String, String>> appliesTo(final Request request) {
+        return new Predicate<Pair<String, String>>() {
+            public boolean matches(Pair<String, String> extensionAndReplacementMimeType) {
+                Matches actual = fileExtension(request);
+                String expected = extensionAndReplacementMimeType.first();
+                return !actual.isEmpty() && ("." + expected).equals(actual.head().group(1));
+            }
+        };
+    }
+
+    private static Matches fileExtension(Request request) {
+        return FILE_EXTENSION.findMatches(request.url().toString());
+    }
+
+    private Callable2<? super Request, ? super Pair<String, String>, Request> applyReplacement() {
         return new Callable2<Request, Pair<String, String>, Request>() {
             public Request call(Request request, Pair<String, String> extensionAndReplacementMimeType) throws Exception {
-                if (hasExtension(fileExtension, extensionAndReplacementMimeType.first())) {
-                    request.headers().remove(HttpHeaders.ACCEPT);
-                    request.headers().add(HttpHeaders.ACCEPT, extensionAndReplacementMimeType.second());
-                    request.url(removeExtension(request.url(), fileExtension));
-                }
+
+                request.headers().remove(HttpHeaders.ACCEPT);
+                request.headers().add(HttpHeaders.ACCEPT, extensionAndReplacementMimeType.second());
+                request.url(removeExtension(request.url(), fileExtension(request)));
                 return request;
             }
         };
@@ -50,10 +83,6 @@ public class ConvertExtensionToAcceptHeader implements HttpHandler {
         MatchResult match = fileExtension.head();
         String original = originalUrl.toString();
         return url(original.substring(0, match.start(1)) + original.substring(match.start(1) + match.group(1).length()));
-    }
-
-    private static boolean hasExtension(Matches actual, String expected) {
-        return !actual.isEmpty() && ("." + expected).equals(actual.head().group(1));
     }
 
     public static class Replacements implements Iterable<Pair<String, String>> {
