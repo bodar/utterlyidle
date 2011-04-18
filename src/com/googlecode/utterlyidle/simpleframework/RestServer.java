1d0011692c196448433243f535d69cee80c00b70
Laszlo Kiss <pityq1@gmail.com>
2011-04-18 17:25:56 +0100

    Enforced that Server closes Application

--- src/com/googlecode/utterlyidle/simpleframework/RestServer.java
+++ src/com/googlecode/utterlyidle/simpleframework/RestServer.java
@@ -1,16 +1,15 @@
 package com.googlecode.utterlyidle.simpleframework;
 
-import com.googlecode.utterlyidle.Application;
-import com.googlecode.utterlyidle.BasePath;
-import com.googlecode.utterlyidle.RestApplication;
-import com.googlecode.utterlyidle.Server;
+import com.googlecode.utterlyidle.*;
 import com.googlecode.utterlyidle.httpserver.HelloWorld;
+import com.googlecode.utterlyidle.jetty.RestApplicationActivator;
 import com.googlecode.utterlyidle.modules.RequestInstanceModule;
 import com.googlecode.utterlyidle.modules.SingleResourceModule;
 import org.simpleframework.http.core.Container;
 import org.simpleframework.transport.connect.Connection;
 import org.simpleframework.transport.connect.SocketConnection;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -21,26 +20,17 @@ import static java.lang.String.format;
 import static java.lang.System.nanoTime;
 
 public class RestServer implements Server {
-    private Connection connection;
-    private SocketAddress address;
+    private final Connection connection;
+    private final Closeable closableActivator;
 
-    public RestServer(int port, BasePath basePath, Application applcation) throws Exception {
-        long start = nanoTime();
-        Container container = new RestContainer(applcation.add(new RequestInstanceModule(basePath)));
-        connection = new SocketConnection(container);
-        address = new InetSocketAddress(port);
-        start();
-        System.out.println(format("Listening on %s, started SimpleWeb in %s msecs", port, calculateMilliseconds(start, nanoTime())));
+    public RestServer(int port, BasePath basePath, CloseableCallable<Application> applicationActivator) throws Exception {
+        this.closableActivator = applicationActivator;
+        connection = startApp(port, basePath, applicationActivator.call());
     }
 
-    public Server start() throws Exception {
-        connection.connect(address);
-        return this;
-    }
-
-    public Server stop() throws IOException {
+    public void close() throws IOException {
         connection.close();
-        return this;
+        closableActivator.close();
     }
 
     public static void main(String[] args) throws Exception {
@@ -49,7 +39,17 @@ public class RestServer implements Server {
 
     public static class Test extends RestServer {
         public Test() throws Exception {
-            super(8000, basePath("/"), new RestApplication().add(new SingleResourceModule(HelloWorld.class)));
+            super(8000, basePath("/"), new RestApplicationActivator(new SingleResourceModule(HelloWorld.class)));
         }
+    }
+
+    private Connection startApp(int port, BasePath basePath, Application application) throws Exception {
+        long start = nanoTime();
+        Container container = new RestContainer(application.add(new RequestInstanceModule(basePath)));
+        Connection connection = new SocketConnection(container);
+        SocketAddress address = new InetSocketAddress(port);
+        connection.connect(address);
+        System.out.println(format("Listening on %s, started SimpleWeb in %s msecs", port, calculateMilliseconds(start, nanoTime())));
+        return connection;
     }
 }
