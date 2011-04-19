78c436804f1b93801720ae201cf054ccea082d06
Laszlo Kiss <pityq1@gmail.com>
2011-04-19 10:11:03 +0100

    rename test

--- test/com/googlecode/utterlyidle/simpleframework/RestServerTest.java
+++ test/com/googlecode/utterlyidle/simpleframework/RestServerTest.java
@@ -2,12 +2,10 @@ package com.googlecode.utterlyidle.simpleframework;
 
 import com.googlecode.totallylazy.Strings;
 import com.googlecode.utterlyidle.*;
-import org.junit.After;
-import org.junit.AfterClass;
+import com.googlecode.utterlyidle.io.Url;
 import org.junit.Test;
 
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 import static com.googlecode.utterlyidle.BasePath.basePath;
 import static org.hamcrest.CoreMatchers.is;
@@ -15,18 +13,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 
-public class RestContainerTest extends ServerContract {
+public class RestServerTest extends ServerContract {
     protected Server createServer(CloseableCallable<Application> appActivator) throws Exception {
-        return new RestServer(port(), basePath("/"), appActivator);
-    }
-
-    protected int port() {
-        return 8000;
+        return new RestServer(basePath("/"), appActivator);
     }
 
     @Test
     public void willOnlyHandleSingleValueHeadersBecauseSimpleWebDoesntSupportIt() throws Exception {
-        HttpURLConnection urlConnection = (HttpURLConnection) new URL("http://localhost:" + port() + "/echoheaders").openConnection();
+        Url url = server.getUrl();
+        HttpURLConnection urlConnection = (HttpURLConnection) urlOf("echoheaders").openConnection();
         urlConnection.setRequestProperty("accept", "*/*");
         urlConnection.setRequestProperty("someheader", "first value");
         urlConnection.setRequestProperty("someheader", "second value");
@@ -39,7 +34,7 @@ public class RestContainerTest extends ServerContract {
 
     @Test
     public void willNotHandleMultipleAcceptHeaders() throws Exception {
-        HttpURLConnection urlConnection = (HttpURLConnection) new URL("http://localhost:" + port() + "/html").openConnection();
+        HttpURLConnection urlConnection = (HttpURLConnection) urlOf("html").openConnection();
         urlConnection.setRequestProperty("accept", "text/plain");
         urlConnection.setRequestProperty("accept", "text/html");
         urlConnection.setRequestProperty("accept", "text/xml");
