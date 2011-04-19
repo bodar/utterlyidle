68f62fab9cb4a23200b06addd5d0f58b8f6cf202
Laszlo Kiss <pityq1@gmail.com>
2011-04-19 10:09:04 +0100

    Server starts up at unused port when no port specified

--- src/com/googlecode/utterlyidle/httpserver/RestServer.java
+++ src/com/googlecode/utterlyidle/httpserver/RestServer.java
@@ -1,6 +1,7 @@
 package com.googlecode.utterlyidle.httpserver;
 
 import com.googlecode.utterlyidle.*;
+import com.googlecode.utterlyidle.io.Url;
 import com.googlecode.utterlyidle.jetty.RestApplicationActivator;
 import com.googlecode.utterlyidle.modules.RequestInstanceModule;
 import com.googlecode.utterlyidle.modules.SingleResourceModule;
@@ -8,10 +9,13 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 import static com.googlecode.totallylazy.callables.TimeCallable.calculateMilliseconds;
 import static com.googlecode.utterlyidle.BasePath.basePath;
+import static com.googlecode.utterlyidle.io.Url.*;
+import static com.googlecode.utterlyidle.io.Url.url;
 import static java.lang.String.format;
 import static java.lang.System.nanoTime;
 import static java.util.concurrent.Executors.newFixedThreadPool;
@@ -19,10 +23,15 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
 public class RestServer implements Server {
     private HttpServer server;
     private final Closeable closableActivator;
+    private Url url;
 
     public RestServer(int port, BasePath basePath, CloseableCallable<Application> applicationActivator) throws Exception {
         this.closableActivator = applicationActivator;
         server = startApp(port, basePath, applicationActivator.call());
+    }
+
+    public RestServer(BasePath basePath, CloseableCallable<Application> applicationActivator) throws Exception {
+        this(0, basePath, applicationActivator);
     }
 
     public void close() throws IOException {
@@ -40,7 +49,12 @@ public class RestServer implements Server {
         server.createContext(basePath.toString(), new RestHandler(application.add(new RequestInstanceModule(basePath))));
         server.setExecutor(newFixedThreadPool(50));
         server.start();
-        System.out.println(format("Listening on %s, started HttpServer in %s msecs", port, calculateMilliseconds(start, nanoTime())));
+        url = url(format("http://%s:%s%s", server.getAddress().getAddress().getHostAddress(), server.getAddress().getPort(), basePath));
+        System.out.println(format("Listening on %s, started HttpServer in %s msecs", url, calculateMilliseconds(start, nanoTime())));
         return server;
+    }
+
+    public Url getUrl() {
+        return url;
     }
 }
--- test/com/googlecode/utterlyidle/jetty/RestServerTest.java
+++ test/com/googlecode/utterlyidle/jetty/RestServerTest.java
@@ -4,16 +4,12 @@ import com.googlecode.utterlyidle.Application;
 import com.googlecode.utterlyidle.CloseableCallable;
 import com.googlecode.utterlyidle.Server;
 import com.googlecode.utterlyidle.ServerContract;
-import org.junit.AfterClass;
 
 import static com.googlecode.utterlyidle.BasePath.basePath;
 
 public class RestServerTest extends ServerContract {
     protected Server createServer(final CloseableCallable<Application> activator) throws Exception {
-        return new RestServer(port(), basePath("/"), activator);
+        return new RestServer(basePath("/"), activator);
     }
 
-    protected int port() {
-        return 8002;
-    }
 }
