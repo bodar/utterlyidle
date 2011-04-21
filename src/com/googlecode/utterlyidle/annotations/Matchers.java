bf1b0d56374ff9861b262d7623368640a3fee679
Daniel Wothington-Bodart <dan@bodar.com>
2011-04-21 12:07:40 +0100

    Seperated usage of annotations in ParametersExtractor

--- src/com/googlecode/utterlyidle/annotations/Matchers.java
+++ src/com/googlecode/utterlyidle/annotations/Matchers.java
@@ -1,13 +1,21 @@
 package com.googlecode.utterlyidle.annotations;
 
 import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Option;
+import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.Sequence;
 import com.googlecode.utterlyidle.*;
+import com.googlecode.utterlyidle.ParametersExtractor;
+import com.googlecode.utterlyidle.cookies.CookieParameters;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 
+import static com.googlecode.totallylazy.Option.none;
+import static com.googlecode.totallylazy.Option.some;
 import static com.googlecode.totallylazy.Predicates.notNullValue;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.utterlyidle.annotations.Param.toParam;
@@ -37,6 +45,37 @@ public class Matchers {
 
     public static HttpMethodActivator httpMethodActivator(String httpMethod, Method method, Application application) {
         UriTemplate uriTemplate = new UriTemplateExtractor().extract(method);
-        return new HttpMethodActivator(method, uriTemplate, httpMethod, consumesMimeMatcher(method), producesMimeMatcher(method), new ArgumentsExtractor(method, uriTemplate, application), new PriorityExtractor().extract(method));
+        return new HttpMethodActivator(method, uriTemplate, httpMethod, consumesMimeMatcher(method), producesMimeMatcher(method), new ParametersExtractor(uriTemplate, application, extractTypesAndNames(method)), new PriorityExtractor().extract(method));
     }
+
+    private static Sequence<Pair<Type, Option<NamedParameter>>> extractTypesAndNames(Method method) {
+        return sequence(method.getGenericParameterTypes()).zip(convertToAnnotationsToNamedParameter(method));
+    }
+
+    private static Sequence<Option<NamedParameter>> convertToAnnotationsToNamedParameter(final Method method) {
+        return sequence(method.getParameterAnnotations()).map(new Callable1<Annotation[], Option<NamedParameter>>() {
+            public Option<NamedParameter> call(Annotation[] annotations) throws Exception {
+                for (final Param param : sequence(annotations).map(toParam())) {
+                    if (param.annotation() instanceof QueryParam) {
+                        return some(new NamedParameter(param.<String>value(), QueryParameters.class));
+                    }
+                    if (param.annotation() instanceof FormParam) {
+                        return some(new NamedParameter(param.<String>value(), FormParameters.class));
+
+                    }
+                    if (param.annotation() instanceof PathParam) {
+                        return some(new NamedParameter(param.<String>value(), PathParameters.class));
+                    }
+                    if (param.annotation() instanceof HeaderParam) {
+                        return some(new NamedParameter(param.<String>value(), HeaderParameters.class));
+                    }
+                    if (param.annotation() instanceof CookieParam) {
+                        return some(new NamedParameter(param.<String>value(), CookieParameters.class));
+                    }
+                }
+                return none();
+            }
+        });
+    }
+
 }
