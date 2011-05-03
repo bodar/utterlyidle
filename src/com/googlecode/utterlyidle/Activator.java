bc14e1c0051dc5338224d94e67a8652010e3071d
Daniel Worthington-Bodart <dan@bodar.com>
2011-05-03 15:31:35 +0100

    Moved activation into BaseHandler

--- src/com/googlecode/utterlyidle/Activator.java
+++ src/com/googlecode/utterlyidle/Activator.java
@@ -1,13 +1,10 @@
 package com.googlecode.utterlyidle;
 
 import com.googlecode.totallylazy.Callable1;
-import com.googlecode.yadic.Resolver;
 
 import java.lang.reflect.Method;
 
 public interface Activator {
-    Response activate(Resolver resolver, Request request, Application application) throws Exception;
-
     Method method();
 
     HttpSignature httpSignature();
--- src/com/googlecode/utterlyidle/HttpMethodActivator.java
+++ src/com/googlecode/utterlyidle/HttpMethodActivator.java
@@ -1,17 +1,8 @@
 package com.googlecode.utterlyidle;
 
-import com.googlecode.totallylazy.Either;
 import com.googlecode.totallylazy.Sequences;
-import com.googlecode.yadic.Resolver;
 
-import javax.ws.rs.core.HttpHeaders;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
-import static com.googlecode.totallylazy.Exceptions.toException;
-import static com.googlecode.utterlyidle.Responses.response;
-import static com.googlecode.yadic.resolvers.Resolvers.create;
-import static com.googlecode.yadic.resolvers.Resolvers.resolve;
 
 public class HttpMethodActivator implements Activator {
     private final HttpSignature httpSignature;
@@ -24,33 +15,6 @@ public class HttpMethodActivator implements Activator {
 
     public HttpSignature httpSignature() {
         return httpSignature;
-    }
-
-
-    public Response activate(Resolver resolver, Request request, Application application) throws Exception {
-        Class<?> declaringClass = method.getDeclaringClass();
-        Object instance = resolve(create(declaringClass, resolver), declaringClass);
-        Object result = getResponse(request, instance, application);
-        if (result instanceof Response) {
-            return (Response) result;
-        }
-        if (result instanceof Either) {
-            result = ((Either) result).value();
-        }
-
-        return response().
-                header(HttpHeaders.CONTENT_TYPE, httpSignature.produces()).
-                entity(result).
-                status(Status.OK);
-    }
-
-    private Object getResponse(Request request, Object instance, Application application) throws Exception {
-        try {
-            Object[] arguments = new ParametersExtractor(httpSignature().uriTemplate(), application, httpSignature().parameters()).extract(request);
-            return method.invoke(instance, arguments);
-        } catch (InvocationTargetException e) {
-            throw toException(e.getCause());
-        }
     }
 
     @Override
