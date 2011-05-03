67fe9c823788ff88dcb0d8ca87259850af39fd44
Daniel Worthington-Bodart <dan@bodar.com>
2011-05-03 13:53:37 +0100

    Continued seperating HttpSignature from activation

--- src/com/googlecode/utterlyidle/Activator.java
+++ src/com/googlecode/utterlyidle/Activator.java
@@ -1,27 +1,13 @@
 package com.googlecode.utterlyidle;
 
-import com.googlecode.totallylazy.Predicate;
 import com.googlecode.yadic.Resolver;
 
 import java.lang.reflect.Method;
 
 public interface Activator {
-    float matchQuality(Request request);
-
     Response activate(Resolver resolver, Request request, Application application) throws Exception;
 
-    Predicate<Request> pathMatcher(BasePath basePath);
-
-    Predicate<Request>  methodMatcher();
-
-    Predicate<Request>  consumesMatcher();
-
-    Predicate<Request>  producesMatcher();
-
-    Predicate<Request> parameterMatcher(Application application);
-
     Method method();
-
 
     HttpSignature httpSignature();
 }
--- src/com/googlecode/utterlyidle/HttpMethodActivator.java
+++ src/com/googlecode/utterlyidle/HttpMethodActivator.java
@@ -1,7 +1,6 @@
 package com.googlecode.utterlyidle;
 
 import com.googlecode.totallylazy.Either;
-import com.googlecode.totallylazy.Predicate;
 import com.googlecode.totallylazy.Sequences;
 import com.googlecode.yadic.Resolver;
 
@@ -27,10 +26,6 @@ public class HttpMethodActivator implements Activator {
         return httpSignature;
     }
 
-    public float matchQuality(Request request) {
-        return producesMatcher().matchQuality(request);
-    }
-
 
     public Response activate(Resolver resolver, Request request, Application application) throws Exception {
         Class<?> declaringClass = method.getDeclaringClass();
@@ -44,47 +39,23 @@ public class HttpMethodActivator implements Activator {
         }
 
         return response().
-                header(HttpHeaders.CONTENT_TYPE, producesMatcher().mimeType()).
+                header(HttpHeaders.CONTENT_TYPE, httpSignature.produces()).
                 entity(result).
                 status(Status.OK);
     }
 
     private Object getResponse(Request request, Object instance, Application application) throws Exception {
         try {
-            Object[] arguments = parameterExtractor(application).extract(request);
+            Object[] arguments = new ParametersExtractor(httpSignature().uriTemplate(), application, httpSignature().arguments()).extract(request);
             return method.invoke(instance, arguments);
         } catch (InvocationTargetException e) {
             throw toException(e.getCause());
         }
     }
 
-    private ParametersExtractor parameterExtractor(Application application) {
-        return new ParametersExtractor(httpSignature().uriTemplate(), application, httpSignature().arguments());
-    }
-
-    public Predicate<Request> pathMatcher(BasePath basePath) {
-        return new PathMatcher(basePath, httpSignature().uriTemplate());
-    }
-
-    public Predicate<Request> methodMatcher() {
-        return new MethodMatcher(httpSignature().httpMethod());
-    }
-
-    public Predicate<Request> consumesMatcher() {
-        return new ConsumesMimeMatcher(httpSignature().consumes());
-    }
-
     @Override
     public String toString() {
         return method.getDeclaringClass().getSimpleName() + "." + method.getName() + Sequences.sequence(method.getGenericParameterTypes()).toString("(", ", ", ")");
-    }
-
-    public ProducesMimeMatcher producesMatcher() {
-        return new ProducesMimeMatcher(httpSignature().produces());
-    }
-
-    public Predicate<Request> parameterMatcher(Application application) {
-        return parameterExtractor(application);
     }
 
     public Method method() {
