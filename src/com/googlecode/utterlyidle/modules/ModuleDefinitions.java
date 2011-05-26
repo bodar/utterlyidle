3b27358fe2743354203ebe655a4ff1ce64baa2f6
Daniel Worthington-Bodart <dan@bodar.com>
2011-05-26 09:31:17 +0100

    Cleaned up custom modules to seperate definition interface from concrete

--- src/com/googlecode/utterlyidle/modules/ModuleDefinitions.java
+++ src/com/googlecode/utterlyidle/modules/ModuleDefinitions.java
@@ -1,68 +1,9 @@
 package com.googlecode.utterlyidle.modules;
 
-import com.googlecode.yadic.Container;
+public interface ModuleDefinitions {
+    ModuleDefinitions addApplicationModule(Class<? extends Module> moduleClass);
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+    ModuleDefinitions addRequestModule(Class<? extends Module> moduleClass);
 
-import static com.googlecode.totallylazy.Methods.invoke;
-import static com.googlecode.totallylazy.Sequences.sequence;
-import static com.googlecode.yadic.resolvers.Resolvers.asCallable1;
-
-public class ModuleDefinitions {
-    private final List<Class<? extends Module>> application = new ArrayList<Class<? extends Module>>();
-    private final List<Class<? extends Module>> request = new ArrayList<Class<? extends Module>>();
-    private final List<Class<? extends Module>> argument = new ArrayList<Class<? extends Module>>();
-
-    public ModuleDefinitions addApplicationModule(Class<? extends Module> moduleClass) {
-        application.add(moduleClass);
-        return this;
-    }
-
-    public ModuleDefinitions addRequestModule(Class<? extends Module> moduleClass) {
-        request.add(moduleClass);
-        return this;
-    }
-
-    public ModuleDefinitions addArgumentModule(Class<? extends Module> moduleClass) {
-        argument.add(moduleClass);
-        return this;
-    }
-
-    public ModuleDefinitions activateApplicationModule(Module module, Container applicationScope) {
-        activate(module, applicationScope, Arrays.<Class<? extends Module>>asList(ModuleDefiner.class));
-        activate(module, applicationScope, application);
-        return this;
-    }
-
-    private void activate(Module module, Container container, final Iterable<Class<? extends Module>> classes) {
-        for (Class<? extends Module> aClass : classes) {
-            if (aClass.isInstance(module)) {
-                for (Method method : aClass.getMethods()) {
-                    invoke(method, module, convertToInstances(method.getGenericParameterTypes(), container));
-                }
-            }
-        }
-    }
-
-    private Object[] convertToInstances(Type[] genericParameterTypes, Container container) {
-        return sequence(genericParameterTypes).map(asCallable1(container)).toArray(Object.class);
-    }
-
-    public ModuleDefinitions activateRequestModules(Iterable<Module> modules, Container requestScope) {
-        for (Module module : modules) {
-            activate(module, requestScope, request);
-        }
-        return this;
-    }
-
-    public ModuleDefinitions activateArgumentModules(Iterable<Module> modules, Container argumentScope) {
-        for (Module module : modules) {
-            activate(module, argumentScope, argument);
-        }
-        return this;
-    }
+    ModuleDefinitions addArgumentModule(Class<? extends Module> moduleClass);
 }
