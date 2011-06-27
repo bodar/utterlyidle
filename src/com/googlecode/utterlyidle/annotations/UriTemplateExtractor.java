99b293ce894c5795ffe9cedc0e78d712ebad6260
Daniel Worthington-Bodart <dan@bodar.com>
2011-06-27 21:02:42 +0100

    Cleanup around annotations

--- src/com/googlecode/utterlyidle/annotations/UriTemplateExtractor.java
+++ src/com/googlecode/utterlyidle/annotations/UriTemplateExtractor.java
@@ -11,6 +11,10 @@ import static com.googlecode.totallylazy.Predicates.notNullValue;
 import static com.googlecode.totallylazy.Sequences.sequence;
 
 public class UriTemplateExtractor implements Extractor<Method, UriTemplate> {
+    static UriTemplate uriTemplate(Method method) {
+        return new UriTemplateExtractor().extract(method);
+    }
+
     public UriTemplate extract(Method method) {
         Sequence<Path> paths = sequence(method.getDeclaringClass().getAnnotation(Path.class), method.getAnnotation(Path.class));
         return UriTemplate.uriTemplate(paths.filter(notNullValue()).map(getValue()).toString("/"));
