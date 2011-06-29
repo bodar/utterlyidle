c7133f58e3b75ea4fa00f0ef7419c538a7d2e00c
Daniel Worthington-Bodart <dan@bodar.com>
2011-06-29 22:18:01 +0100

    Added support for generic static resource bindings in a given package

--- src/com/googlecode/utterlyidle/dsl/DslBindings.java
+++ src/com/googlecode/utterlyidle/dsl/DslBindings.java
@@ -2,8 +2,16 @@ package com.googlecode.utterlyidle.dsl;
 
 import com.googlecode.utterlyidle.Binding;
 
+import java.util.concurrent.Callable;
+
+import static com.googlecode.totallylazy.Callers.call;
+
 public class DslBindings {
-    public static Binding binding(BindingBuilder bindingBuilder) {
-        return bindingBuilder.build();
+    public static Binding binding(Callable<Binding> builder) {
+        return call(builder);
+    }
+
+    public static Binding[] bindings(Callable<Binding[]> builder) {
+        return call(builder);
     }
 }
