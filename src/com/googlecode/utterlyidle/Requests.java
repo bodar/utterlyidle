d2909b448d83998f4c811aa8757b45aff613ffe6
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2011-07-12 21:34:57 +0200

    adding Request and Response Callables to extract parts of the Request and Response

--- src/com/googlecode/utterlyidle/Requests.java
+++ src/com/googlecode/utterlyidle/Requests.java
@@ -1,5 +1,7 @@
 package com.googlecode.utterlyidle;
 
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.utterlyidle.io.HierarchicalPath;
 import com.googlecode.utterlyidle.io.Url;
 
 public class Requests {
@@ -9,5 +11,29 @@ public class Requests {
 
     public static Request request(String method, String path, QueryParameters query, HeaderParameters headers, byte[] input) {
         return request(method, Url.url(path + query.toString()), headers, input);
+    }
+
+    public static Callable1<Request, String> method() {
+        return new Callable1<Request, String>() {
+            public String call(Request request) throws Exception {
+                return request.method();
+            }
+        };
+    }
+
+    public static Callable1<Request, QueryParameters> query() {
+        return new Callable1<Request, QueryParameters>() {
+            public QueryParameters call(Request request) throws Exception {
+                return request.query();
+            }
+        };
+    }
+
+    public static Callable1<Request, HierarchicalPath> path() {
+        return new Callable1<Request, HierarchicalPath>() {
+            public HierarchicalPath call(Request request) throws Exception {
+                return request.url().path();
+            }
+        };
     }
 }
