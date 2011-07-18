2d2e8ebb4981289a22cfc25b64eeb30870816a0e
Alex Luker <alex.luker@gmail.com>
2011-07-18 14:10:44 +0100

    adding server side includes

--- src/com/googlecode/utterlyidle/sitemesh/StringTemplateDecorator.java
+++ src/com/googlecode/utterlyidle/sitemesh/StringTemplateDecorator.java
@@ -2,7 +2,9 @@ package com.googlecode.utterlyidle.sitemesh;
 
 import com.googlecode.totallylazy.LazyException;
 import com.googlecode.totallylazy.Maps;
+import com.googlecode.utterlyidle.Application;
 import com.googlecode.utterlyidle.BasePath;
+import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.QueryParameters;
 import org.antlr.stringtemplate.NoIndentWriter;
 import org.antlr.stringtemplate.StringTemplate;
@@ -16,19 +18,19 @@ import static com.googlecode.totallylazy.Sequences.sequence;
 
 public class StringTemplateDecorator implements Decorator {
     private final StringTemplate template;
-    private final Includer includer;
+    private final HttpHandler httpHandlerForIncludes;
     private final BasePath base;
     private final QueryParameters queryParameters;
 
-    public StringTemplateDecorator(StringTemplate template, Includer includer, BasePath base, QueryParameters queryParameters) {
+    public StringTemplateDecorator(StringTemplate template, HttpHandler httpHandlerForIncludes, BasePath base, QueryParameters queryParameters) {
         this.template = template;
-        this.includer = includer;
+        this.httpHandlerForIncludes = httpHandlerForIncludes;
         this.base = base;
         this.queryParameters = queryParameters;
     }
 
     public Decorator setContent(PropertyMap content) throws IOException {
-        template.setAttribute("include", includer);
+        template.setAttribute("include", new PageMap(httpHandlerForIncludes));
         template.setAttribute("base", base);
         template.setAttribute("query", sequence(queryParameters).fold(Maps.<String, List<String>>map(), Maps.<String, String>asMultiValuedMap()));
         template.setAttribute("properties", content);
