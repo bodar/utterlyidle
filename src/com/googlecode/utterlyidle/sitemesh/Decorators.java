cc6e67f92ce3122719bd95e6f282866c7dbf5f30
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-25 11:58:01 +0100

    Introduced SiteMesh modules to enable quickly setting up SiteMesh

--- src/com/googlecode/utterlyidle/sitemesh/Decorators.java
+++ src/com/googlecode/utterlyidle/sitemesh/Decorators.java
@@ -1,10 +1,43 @@
 package com.googlecode.utterlyidle.sitemesh;
 
+import com.googlecode.totallylazy.Callable2;
+import com.googlecode.totallylazy.Pair;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.Response;
 
-public interface Decorators {
-    Decorator getDecoratorFor(Request request, Response response);
+import java.util.ArrayList;
+import java.util.List;
 
-    Decorators add(DecoratorRule rule);
+public class Decorators {
+    private final List<DecoratorRule> rules = new ArrayList<DecoratorRule>();
+    private final DecoratorProvider provider;
+
+    public Decorators(DecoratorProvider provider) {
+        this.provider = provider;
+    }
+
+    public Decorators add(DecoratorRule rule) {
+        rules.add(rule);
+        return this;
+    }
+
+    public Decorator getDecoratorFor(Request request, Response response) {
+        for (DecoratorRule rule : rules) {
+            if (rule.matches(Pair.pair(request, response))) {
+                return provider.get(rule.templateName());
+            }
+        }
+        return new NoneDecorator();
+    }
+
+    public static Callable2<? super Decorators, ? super DecoratorRule, Decorators> add() {
+        return new Callable2<Decorators, DecoratorRule, Decorators>() {
+            public Decorators call(Decorators decorators, DecoratorRule rule) throws Exception {
+                return decorators.add(rule);
+            }
+        };
+    }
+
+
+
 }
