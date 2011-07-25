db28b6776d2045ef8625d61f5684726967ed5b22
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-25 16:35:06 +0100

    Added Exception to module interfaces so implementors don't have to

--- src/com/googlecode/utterlyidle/modules/ApplicationScopedModule.java
+++ src/com/googlecode/utterlyidle/modules/ApplicationScopedModule.java
@@ -3,5 +3,5 @@ package com.googlecode.utterlyidle.modules;
 import com.googlecode.yadic.Container;
 
 public interface ApplicationScopedModule extends Module {
-    Module addPerApplicationObjects(Container container);
+    Module addPerApplicationObjects(Container container) throws Exception;
 }
