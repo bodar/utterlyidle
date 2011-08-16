db005ff7064cc7bae35a875a3adf1175f37f256b
Daniel Worthington-Bodart <dan@bodar.com>
2011-08-16 15:26:11 +0100

    Missing file

--- src/com/googlecode/utterlyidle/modules/DependsOnResolver.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.utterlyidle.modules;
-
-import com.googlecode.yadic.Resolver;
-
-public interface DependsOnResolver {
-    void setResolver(Resolver resolver) throws Exception;
-}
