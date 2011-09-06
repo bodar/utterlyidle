035d3d395a1e006b02acfdb458c721654bf6d862
kwasniewski mateusz <kwasniewski.mateusz@gmail.com>
2011-09-06 14:37:51 +0100

    refactoring towards state independent (BasePath field) callables

--- src/com/googlecode/utterlyidle/AbsoluteLocationHandler.java
+++ src/com/googlecode/utterlyidle/AbsoluteLocationHandler.java
@@ -10,7 +10,7 @@ import static com.googlecode.utterlyidle.HttpHeaders.LOCATION;
 import static java.lang.String.format;
 import static java.util.regex.Pattern.quote;
 
-public class AbsoluteLocationHandler implements HttpHandler{
+public class AbsoluteLocationHandler implements HttpHandler {
     private final HttpHandler httpHandler;
     private final BasePath basePath;
 
@@ -20,8 +20,8 @@ public class AbsoluteLocationHandler implements HttpHandler{
     }
 
     public Response handle(Request request) throws Exception {
-        Response response = httpHandler.handle(removeBasePathFromUri(request));
-        Sequence<Uri> absoluteLocations = sequence(response.headers(LOCATION)).realise().map(asUri()).map(changeToAbsoluteUrl(baseUri(removeBasePathFromUri(request))));
+        Response response = httpHandler.handle(removeBasePathFromUri(request, basePath));
+        Sequence<Uri> absoluteLocations = sequence(response.headers(LOCATION)).realise().map(asAbsoluteUri(request, basePath));
         response.headers().remove(LOCATION);
         for (Uri absoluteLocation : absoluteLocations) {
             response.header(LOCATION, absoluteLocation.toString());
@@ -29,15 +29,43 @@ public class AbsoluteLocationHandler implements HttpHandler{
         return response;
     }
 
-    private Request removeBasePathFromUri(Request request) {
-        return request.uri(replacePath(request.uri(), removeBasePath(request.uri().path())));
+    public static Uri toAbsoluteUri(String absolutePath, Request request, BasePath basePath) {
+        return toAbsoluteUri(Uri.uri(absolutePath), baseUri(removeBasePathFromUri(request, basePath), basePath));
     }
 
-    private Uri replacePath(Uri uri, String value) {
+    public static Callable1<String, Uri> asAbsoluteUri(final Request request, final BasePath basePath) {
+        return new Callable1<String, Uri>() {
+            @Override
+            public Uri call(String value) throws Exception {
+                return toAbsoluteUri(value, request, basePath);
+            }
+        };
+    }
+
+    public static Uri toAbsoluteUri(Uri uri, Uri baseUri) {
+        if (uri.isFullyQualified()) {
+            return uri;
+        }
+        return baseUri.path(uri.path()).query(uri.query()).fragment(uri.fragment());
+    }
+
+    public static Callable1<? super Uri, Uri> asAbsoluteUri(final Uri baseUri) {
+        return new Callable1<Uri, Uri>() {
+            public Uri call(Uri uri) throws Exception {
+                return toAbsoluteUri(uri, baseUri);
+            }
+        };
+    }
+
+    private static Request removeBasePathFromUri(Request request, BasePath basePath) {
+        return request.uri(replacePath(request.uri(), removeBasePath(request.uri().path(), basePath)));
+    }
+
+    private static Uri replacePath(Uri uri, String value) {
         return uri.path(value);
     }
 
-    private String removeBasePath(String path) {
+    private static String removeBasePath(String path, BasePath basePath) {
         String regex = "(" + quote(basePath.toString()) + ")";
         return path.replaceFirst(regex, "/");
     }
@@ -51,24 +79,11 @@ public class AbsoluteLocationHandler implements HttpHandler{
         };
     }
 
-    private Uri baseUri(Request request) {
+    private static Uri baseUri(Request request, BasePath basePath) {
         String host = request.headers().getValue(HttpHeaders.HOST);
-        if(host == null){
+        if (host == null) {
             return uri(basePath.toString());
         }
         return uri(format("http://%s%s", host, basePath));
     }
-
-    public static Callable1<? super Uri, Uri> changeToAbsoluteUrl(final Uri baseUri) {
-        return new Callable1<Uri, Uri>() {
-            public Uri call(Uri uri) throws Exception {
-                if(uri.isFullyQualified()){
-                    return uri;
-                }
-                return baseUri.path(uri.path()).query(uri.query()).fragment(uri.fragment());
-            }
-        };
-    }
-
-
 }
