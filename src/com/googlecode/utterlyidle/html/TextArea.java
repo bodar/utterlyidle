aa66f47e77b58fa8ffe0a4a5f570d237d6f9c00b
Daniel Worthington-Bodart <dan@bodar.com>
2011-09-20 14:42:01 +0100

    Imported BGB Html testing

---
+++ src/com/googlecode/utterlyidle/html/TextArea.java
@@ -0,0 +1,27 @@
+package com.googlecode.utterlyidle.html;
+
+import org.w3c.dom.Element;
+
+import static com.googlecode.totallylazy.records.xml.Xml.selectContents;
+
+public class TextArea implements NameValue{
+    public static final String NAME = "@name";
+    protected final Element textArea;
+
+    public TextArea(Element textArea) {
+        this.textArea = textArea;
+    }
+
+    public String value() {
+        return textArea.getTextContent();
+    }
+
+    public TextArea value(String value) {
+        textArea.setTextContent(value);
+        return this;
+    }
+
+    public String name() {
+        return selectContents(textArea, NAME);
+    }
+}
