fbdaa1aac8f01605a2d4d07f7c9fef846ac9246c
Daniel Worthington-Bodart <dan@bodar.com>
2011-10-06 17:42:45 +0100

    Added CachingModule

---
+++ src/com/googlecode/utterlyidle/modules/CachingModule.java
@@ -0,0 +1,32 @@
+package com.googlecode.utterlyidle.modules;
+
+import com.googlecode.utterlyidle.HttpHandler;
+import com.googlecode.utterlyidle.MediaType;
+import com.googlecode.utterlyidle.handlers.CacheControlHandler;
+import com.googlecode.utterlyidle.handlers.CachePolicy;
+import com.googlecode.yadic.Container;
+
+import static com.googlecode.totallylazy.Predicates.or;
+import static com.googlecode.utterlyidle.handlers.CachePolicy.cachePolicy;
+import static com.googlecode.utterlyidle.sitemesh.ContentTypePredicate.contentType;
+
+public class CachingModule implements ApplicationScopedModule, RequestScopedModule {
+    @Override
+    public Module addPerApplicationObjects(Container container) throws Exception {
+        container.addInstance(CachePolicy.class, cachePolicy(60, or(
+                contentType(MediaType.IMAGE_PNG),
+                contentType(MediaType.IMAGE_GIF),
+                contentType(MediaType.IMAGE_JPEG),
+                contentType(MediaType.IMAGE_X_ICON),
+                contentType(MediaType.TEXT_JAVASCRIPT),
+                contentType(MediaType.TEXT_CSS)
+        )));
+        return this;
+    }
+
+    @Override
+    public Module addPerRequestObjects(Container container) throws Exception {
+        container.decorate(HttpHandler.class, CacheControlHandler.class);
+        return this;
+    }
+}
