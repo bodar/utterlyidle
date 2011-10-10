0948b27ca6dec861aef7f69805f2e99027582a0f
Daniel Worthington-Bodart <dan@bodar.com>
2011-10-10 11:51:16 +0100

    Made Cache and Etag handlers only work for 200 Responses

--- test/com/googlecode/utterlyidle/modules/EtagHandlerTest.java
+++ test/com/googlecode/utterlyidle/modules/EtagHandlerTest.java
@@ -11,7 +11,8 @@ import static com.googlecode.utterlyidle.HttpHeaders.IF_NONE_MATCH;
 import static com.googlecode.utterlyidle.RequestBuilder.get;
 import static com.googlecode.utterlyidle.RequestBuilder.post;
 import static com.googlecode.utterlyidle.Responses.response;
-import static com.googlecode.utterlyidle.Status.NO_CONTENT;
+import static com.googlecode.utterlyidle.Status.OK;
+import static com.googlecode.utterlyidle.Status.SEE_OTHER;
 import static com.googlecode.utterlyidle.handlers.ReturnResponseHandler.returnsResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -32,11 +33,16 @@ public class EtagHandlerTest {
     }
 
     @Test
-    public void onlyAppliesEtagToGetRequests() throws Exception{
-        HttpHandler handler = new EtagHandler(returnsResponse(response().bytes("abc".getBytes("UTF-8"))));
+    public void onlyAppliesEtagToGetRequestsWithOkResponse() throws Exception{
+        HttpHandler handler = new EtagHandler(returnsResponse(response(OK).bytes("abc".getBytes("UTF-8"))));
         Response response = handler.handle(post("/").build());
         assertThat(response.headers().contains(ETAG), is(false));
         assertThat(response.headers().contains(Content_MD5), is(false));
+
+        HttpHandler handler1 = new EtagHandler(returnsResponse(response(SEE_OTHER).bytes("abc".getBytes("UTF-8"))));
+        Response response1 = handler1.handle(get("/").build());
+        assertThat(response1.headers().contains(ETAG), is(false));
+        assertThat(response1.headers().contains(Content_MD5), is(false));
     }
 
     @Test
@@ -45,14 +51,5 @@ public class EtagHandlerTest {
         Response response = handler.handle(get("/").withHeader(IF_NONE_MATCH, "\"900150983cd24fb0d6963f7d28e17f72\"").build());
         assertThat(response.status(), is(Status.NOT_MODIFIED));
         assertThat(response.bytes().length, is(0));
-    }
-
-    @Test
-    public void supportsNoContent() throws Exception{
-        HttpHandler handler = new EtagHandler(returnsResponse(response(NO_CONTENT)));
-        Response response = handler.handle(get("/").build());
-        assertThat(response.header(ETAG), is("\"d41d8cd98f00b204e9800998ecf8427e\""));
-        assertThat(response.header(Content_MD5), is("1B2M2Y8AsgTpgAmY7PhCfg=="));
-
     }
 }
