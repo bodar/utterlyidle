5c2235d5627f2596b0f4e7db34a35374106f5d5e
dan <dan@bodar.com>
2011-10-31 15:08:00 +0000

    Cleaned up CachePolicy

--- src/com/googlecode/utterlyidle/handlers/CachePolicy.java
+++ src/com/googlecode/utterlyidle/handlers/CachePolicy.java
@@ -3,38 +3,31 @@ package com.googlecode.utterlyidle.handlers;
 import com.googlecode.totallylazy.Pair;
 import com.googlecode.totallylazy.Predicate;
 import com.googlecode.totallylazy.Predicates;
-import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.Value;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.Response;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static com.googlecode.totallylazy.Predicates.always;
 import static com.googlecode.totallylazy.Sequences.sequence;
-import static java.util.Arrays.asList;
 
 public class CachePolicy implements Value<Integer>, Predicate<Pair<Request, Response>> {
     private final int seconds;
-    private final List<Predicate<? super Pair<Request, Response>>> predicates = new ArrayList<Predicate<? super Pair<Request, Response>>>();
+    private final List<Predicate<Pair<Request, Response>>> predicates = new ArrayList<Predicate<Pair<Request, Response>>>();
 
-    public CachePolicy(int seconds, Predicate<? super Pair<Request, Response>> predicate) {
+    @SuppressWarnings("unchecked")
+    public CachePolicy(int seconds) {
         this.seconds = seconds;
-        add(predicate);
-    }
-
-    public static CachePolicy cachePolicy(final int seconds, Predicate<? super Pair<Request, Response>> predicate) {
-        return new CachePolicy(seconds, predicate);
     }
 
     public static CachePolicy cachePolicy(final int seconds) {
-        return new CachePolicy(seconds, always());
+        return new CachePolicy(seconds);
     }
 
-    public void add(Predicate<? super Pair<Request, Response>>... predicate) {
-        predicates.addAll(asList(predicate));
+    public CachePolicy add(Predicate<? super Pair<Request, Response>> predicate) {
+        predicates.add((Predicate<Pair<Request, Response>>) predicate);
+        return this;
     }
 
     @Override
@@ -46,5 +39,4 @@ public class CachePolicy implements Value<Integer>, Predicate<Pair<Request, Resp
     public boolean matches(Pair<Request, Response> pair) {
         return sequence(predicates).exists(Predicates.matches(pair));
     }
-
 }
