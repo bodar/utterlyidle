1e087212c095a425e47409cca6b7165da662e38b
spmiller <spmiller@gmail.com>
2011-11-14 17:04:31 +0000

    Add a resource that can display the last 20 exceptions

---
+++ src/com/googlecode/utterlyidle/rendering/exceptions/LastExceptionsModule.java
@@ -0,0 +1,47 @@
+package com.googlecode.utterlyidle.rendering.exceptions;
+
+import com.googlecode.funclate.Model;
+import com.googlecode.funclate.stringtemplate.EnhancedStringTemplateGroup;
+import com.googlecode.totallylazy.Predicates;
+import com.googlecode.utterlyidle.Renderer;
+import com.googlecode.utterlyidle.Resources;
+import com.googlecode.utterlyidle.handlers.ResponseHandlers;
+import com.googlecode.utterlyidle.modules.ApplicationScopedModule;
+import com.googlecode.utterlyidle.modules.Module;
+import com.googlecode.utterlyidle.modules.ResourcesModule;
+import com.googlecode.utterlyidle.modules.ResponseHandlersModule;
+import com.googlecode.yadic.Container;
+
+import static com.googlecode.totallylazy.Predicates.where;
+import static com.googlecode.totallylazy.URLs.packageUrl;
+import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.annotatedClass;
+import static com.googlecode.utterlyidle.handlers.HandlerRule.entity;
+import static com.googlecode.utterlyidle.handlers.RenderingResponseHandler.renderer;
+
+public class LastExceptionsModule implements ResourcesModule, ApplicationScopedModule, ResponseHandlersModule {
+
+    @Override
+    public Module addPerApplicationObjects(Container container) throws Exception {
+        container.addInstance(LastExceptions.class, new LastExceptions(20));
+        return this;
+    }
+
+    @Override
+    public Module addResources(Resources resources) throws Exception {
+        resources.add(annotatedClass(LastExceptionsResource.class));
+        return this;
+    }
+
+    @Override
+    public Module addResponseHandlers(ResponseHandlers handlers) throws Exception {
+        handlers.add(where(entity(Model.class), Predicates.<Model>instanceOf(Model.class)), renderer(new Renderer<Model>() {
+            @Override
+            public String render(Model model) throws Exception {
+                EnhancedStringTemplateGroup group = new EnhancedStringTemplateGroup(packageUrl(LastExceptionsResource.class));
+                return group.getInstanceOf("lastExceptions", model.toMap()).toString();
+            }
+        }));
+
+        return this;
+    }
+}
