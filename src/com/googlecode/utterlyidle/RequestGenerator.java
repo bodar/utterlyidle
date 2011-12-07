6cd27d31e6d417fac3b56f6182fea9469d9724aa
kwasniewski.mateusz <kwasniewski.mateusz@localhost>
2011-12-07 16:30:42 +0000

    removing extra generics from Invocation in the RequestGenerator

--- src/com/googlecode/utterlyidle/RequestGenerator.java
+++ src/com/googlecode/utterlyidle/RequestGenerator.java
@@ -3,6 +3,6 @@ package com.googlecode.utterlyidle;
 import com.googlecode.totallylazy.proxy.Invocation;
 
 public interface RequestGenerator {
-    public Request requestFor(Invocation<Object, String> invocation);
-    public Request requestFor(Binding binding, Object... arguments);
+    public Request requestFor(final Invocation invocation);
+    public Request requestFor(final Binding binding, final Object... arguments);
 }
