e7614dbfb7c10cdf4bfbc07817f01d483f562f4f
Mateusz Kwasniewski <Mateusz Kwasniewski@localhost>
2011-12-09 13:49:50 +0000

    no decorator when profiling

--- src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
@@ -1,6 +1,5 @@
 package com.googlecode.utterlyidle.profiling;
 
-import com.googlecode.totallylazy.predicates.LogicalPredicate;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.Resources;
 import com.googlecode.utterlyidle.handlers.Auditors;
@@ -9,15 +8,20 @@ import com.googlecode.utterlyidle.modules.AuditModule;
 import com.googlecode.utterlyidle.modules.Module;
 import com.googlecode.utterlyidle.modules.ResourcesModule;
 import com.googlecode.utterlyidle.modules.ResponseHandlersModule;
+import com.googlecode.utterlyidle.sitemesh.Decorators;
+import com.googlecode.utterlyidle.sitemesh.SiteMeshModule;
 
 import static com.googlecode.totallylazy.Callables.first;
-import static com.googlecode.totallylazy.Predicates.where;
-import static com.googlecode.utterlyidle.Requests.query;
+import static com.googlecode.totallylazy.Predicates.*;
+import static com.googlecode.utterlyidle.Requests.hasQueryParameter;
+import static com.googlecode.utterlyidle.Requests.queryParameter;
 import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.annotatedClass;
 import static com.googlecode.utterlyidle.handlers.RenderingResponseHandler.renderer;
 import static com.googlecode.utterlyidle.profiling.FunclateModelRenderer.funclateModelRenderer;
+import static com.googlecode.utterlyidle.sitemesh.StaticDecoratorRule.staticRule;
+import static com.googlecode.utterlyidle.sitemesh.TemplateName.templateName;
 
-public class ProfilingModule implements ResourcesModule, ResponseHandlersModule, AuditModule {
+public class ProfilingModule implements ResourcesModule, ResponseHandlersModule, AuditModule, SiteMeshModule {
 
     @Override
     public Module addResources(Resources resources) throws Exception {
@@ -37,12 +41,10 @@ public class ProfilingModule implements ResourcesModule, ResponseHandlersModule,
         return auditors;
     }
 
-    public static LogicalPredicate<Request> hasQueryParameter(final String parameter) {
-        return new LogicalPredicate<Request>() {
-            @Override
-            public boolean matches(Request request) {
-                return query(request).contains(parameter);
-            }
-        };
+    @Override
+    public Decorators addDecorators(Decorators decorators) {
+        decorators.add(staticRule(where(first(Request.class), where(queryParameter("profile"), is(notNullValue()))), templateName("none")));
+        return decorators;
     }
+
 }
