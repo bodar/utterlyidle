814b86ea19a28c3f3f8ff911bdbdd1ffc0ccee75
Mateusz Kwasniewski <Mateusz Kwasniewski@localhost>
2011-12-09 15:00:57 +0000

    Moving profiling from a resource to a handler

--- src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
@@ -1,50 +1,35 @@
 package com.googlecode.utterlyidle.profiling;
 
+import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.Request;
-import com.googlecode.utterlyidle.Resources;
 import com.googlecode.utterlyidle.handlers.Auditors;
-import com.googlecode.utterlyidle.handlers.ResponseHandlers;
 import com.googlecode.utterlyidle.modules.AuditModule;
 import com.googlecode.utterlyidle.modules.Module;
-import com.googlecode.utterlyidle.modules.ResourcesModule;
-import com.googlecode.utterlyidle.modules.ResponseHandlersModule;
+import com.googlecode.utterlyidle.modules.RequestScopedModule;
 import com.googlecode.utterlyidle.sitemesh.Decorators;
 import com.googlecode.utterlyidle.sitemesh.SiteMeshModule;
+import com.googlecode.yadic.Container;
 
 import static com.googlecode.totallylazy.Callables.first;
 import static com.googlecode.totallylazy.Predicates.*;
-import static com.googlecode.utterlyidle.Requests.hasQueryParameter;
 import static com.googlecode.utterlyidle.Requests.queryParameter;
-import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.annotatedClass;
-import static com.googlecode.utterlyidle.handlers.RenderingResponseHandler.renderer;
-import static com.googlecode.utterlyidle.profiling.FunclateModelRenderer.funclateModelRenderer;
 import static com.googlecode.utterlyidle.sitemesh.StaticDecoratorRule.staticRule;
 import static com.googlecode.utterlyidle.sitemesh.TemplateName.templateName;
 
-public class ProfilingModule implements ResourcesModule, ResponseHandlersModule, AuditModule, SiteMeshModule {
-
+public class ProfilingModule implements RequestScopedModule, AuditModule, SiteMeshModule {
     @Override
-    public Module addResources(Resources resources) throws Exception {
-        resources.add(annotatedClass(ProfilingResource.class));
-        return this;
-    }
-
-    @Override
-    public Module addResponseHandlers(ResponseHandlers handlers) throws Exception {
-        handlers.add(where(first(Request.class), hasQueryParameter("profile")), renderer(funclateModelRenderer(ProfilingResource.class)));
+    public Module addPerRequestObjects(Container container) throws Exception {
+        container.decorate(HttpHandler.class, ProfilingHandler.class);
         return this;
     }
 
     @Override
     public Auditors addAuditors(Auditors auditors) throws Exception {
-        auditors.add(ProfilingAuditor.class);
-        return auditors;
+        return auditors.add(ProfilingAuditor.class);
     }
 
     @Override
     public Decorators addDecorators(Decorators decorators) {
-        decorators.add(staticRule(where(first(Request.class), where(queryParameter("profile"), is(notNullValue()))), templateName("none")));
-        return decorators;
+        return decorators.add(staticRule(where(first(Request.class), where(queryParameter("profile"), is(notNullValue()))), templateName("none")));
     }
-
 }
