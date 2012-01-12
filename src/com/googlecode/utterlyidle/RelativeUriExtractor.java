95552e1766ff3c282e34b650388e65089be447e6
jozefdransfield <jozefdransfield@gmail.com>
2012-01-12 14:43:33 +0000

    Redirector supports building urls from a QueryParameters object

--- src/com/googlecode/utterlyidle/RelativeUriExtractor.java
+++ src/com/googlecode/utterlyidle/RelativeUriExtractor.java
@@ -1,14 +1,21 @@
 package com.googlecode.utterlyidle;
 
-import com.googlecode.totallylazy.Uri;
+import com.googlecode.totallylazy.*;
+
+import static com.googlecode.totallylazy.Sequences.flatten;
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.utterlyidle.QueryParameters.queryParameters;
 
 public class RelativeUriExtractor {
     public static Uri relativeUriOf(Binding binding, Object... arguments) {
         PathParameters parameters = Extractors.extractParameters(binding, arguments, new PathParameters());
         String path = binding.uriTemplate().generate(parameters).toString();
         QueryParameters query = Extractors.extractParameters(binding, arguments, new QueryParameters());
-        return Uri.uri(path + query.toString());
+        return Uri.uri(path + extractParametersFromQueryArgument(arguments, query).toString());
     }
 
+    private static QueryParameters extractParametersFromQueryArgument(Object[] arguments, QueryParameters sourceParameters) {
+        return queryParameters(flatten(sequence(arguments).safeCast(QueryParameters.class)).join(sequence(sourceParameters)));
+    }
 
 }
