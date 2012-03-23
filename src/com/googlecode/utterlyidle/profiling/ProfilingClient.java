3b2b158f5303d6489b99bc82f8421bad45b8e707
Daniel Worthington-Bodart <dan@bodar.com>
2012-03-23 15:02:08 +0000

    Removed ThreadLocal from profiling

---
+++ src/com/googlecode/utterlyidle/profiling/ProfilingClient.java
@@ -0,0 +1,31 @@
+package com.googlecode.utterlyidle.profiling;
+
+import com.googlecode.utterlyidle.HttpHandler;
+import com.googlecode.utterlyidle.Request;
+import com.googlecode.utterlyidle.Response;
+import com.googlecode.utterlyidle.handlers.HttpClient;
+
+import static com.googlecode.totallylazy.callables.TimeCallable.calculateMilliseconds;
+import static com.googlecode.utterlyidle.handlers.HttpClient.methods.httpClient;
+
+public class ProfilingClient implements HttpClient {
+    private final HttpClient httpHandler;
+    private final ProfilingData profilingData;
+
+    public ProfilingClient(HttpClient httpHandler, ProfilingData profilingData) {
+        this.httpHandler = httpHandler;
+        this.profilingData = profilingData;
+    }
+
+    public static ProfilingClient profile(final HttpHandler httpHandler, ProfilingData profilingData) {
+        return new ProfilingClient(httpClient(httpHandler), profilingData);
+    }
+
+    @Override
+    public Response handle(Request request) throws Exception {
+        long start = System.nanoTime();
+        Response response = httpHandler.handle(request);
+        profilingData.add(request.uri(), response.status(), calculateMilliseconds(start, System.nanoTime()));
+        return response;
+    }
+}
--- src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
@@ -1,35 +1,17 @@
 package com.googlecode.utterlyidle.profiling;
 
 import com.googlecode.utterlyidle.HttpHandler;
-import com.googlecode.utterlyidle.Request;
-import com.googlecode.utterlyidle.handlers.Auditors;
-import com.googlecode.utterlyidle.modules.AuditModule;
+import com.googlecode.utterlyidle.handlers.HttpClient;
 import com.googlecode.utterlyidle.modules.Module;
 import com.googlecode.utterlyidle.modules.RequestScopedModule;
-import com.googlecode.utterlyidle.sitemesh.Decorators;
-import com.googlecode.utterlyidle.sitemesh.SiteMeshModule;
 import com.googlecode.yadic.Container;
 
-import static com.googlecode.totallylazy.Callables.first;
-import static com.googlecode.totallylazy.Predicates.*;
-import static com.googlecode.utterlyidle.Requests.queryParameter;
-import static com.googlecode.utterlyidle.sitemesh.StaticDecoratorRule.staticRule;
-import static com.googlecode.utterlyidle.sitemesh.TemplateName.templateName;
-
-public class ProfilingModule implements RequestScopedModule, AuditModule, SiteMeshModule {
+public class ProfilingModule implements RequestScopedModule {
     @Override
     public Module addPerRequestObjects(Container container) throws Exception {
         container.decorate(HttpHandler.class, ProfilingHandler.class);
+        container.decorate(HttpClient.class, ProfilingClient.class);
+        container.add(ProfilingData.class);
         return this;
-    }
-
-    @Override
-    public Auditors addAuditors(Auditors auditors) throws Exception {
-        return auditors.add(ProfilingAuditor.class);
-    }
-
-    @Override
-    public Decorators addDecorators(Decorators decorators) {
-        return decorators.add(staticRule(where(first(Request.class), where(queryParameter("profile"), is(notNullValue()))), templateName("none")));
     }
 }
