9a352595a8fdbb689c2259960d61919889c09d63
dan <dan@bodar.com>
2012-03-23 17:32:52 +0000

    Added links to profiler

--- src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
@@ -1,6 +1,6 @@
 package com.googlecode.utterlyidle.profiling;
 
-import com.googlecode.lazyrecords.Loggers;
+import com.googlecode.lazyrecords.Logger;
 import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.handlers.HttpClient;
 import com.googlecode.utterlyidle.modules.Module;
@@ -10,13 +10,12 @@ import com.googlecode.yadic.Container;
 public class ProfilingModule implements RequestScopedModule {
     @Override
     public Module addPerRequestObjects(Container container) throws Exception {
-        ProfilingData data = new ProfilingData();
-        if (container.contains(Loggers.class)) {
-            container.get(Loggers.class).add(data);
+        container.add(ProfilingData.class);
+        if (container.contains(Logger.class)) {
+            container.decorate(Logger.class, ProfilingLogger.class);
         }
         container.decorate(HttpHandler.class, ProfilingHandler.class);
         container.decorate(HttpClient.class, ProfilingClient.class);
-        container.addInstance(ProfilingData.class, data);
         return this;
     }
 }
