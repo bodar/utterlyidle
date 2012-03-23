c8fc372271f3e35fa2738fbc1c6ddb0f9e40caf1
dan <dan@bodar.com>
2012-03-23 15:02:08 +0000

    Removed ThreadLocal from profiling

--- src/com/googlecode/utterlyidle/profiling/ProfilingHandler.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingHandler.java
@@ -1,74 +1,43 @@
 package com.googlecode.utterlyidle.profiling;
 
-import com.googlecode.funclate.Model;
-import com.googlecode.totallylazy.Callable1;
-import com.googlecode.totallylazy.Triple;
-import com.googlecode.totallylazy.Uri;
-import com.googlecode.utterlyidle.Application;
+import com.googlecode.funclate.stringtemplate.EnhancedStringTemplateGroup;
 import com.googlecode.utterlyidle.HttpHandler;
-import com.googlecode.utterlyidle.Parameters;
-import com.googlecode.utterlyidle.QueryParameters;
 import com.googlecode.utterlyidle.Request;
-import com.googlecode.utterlyidle.Requests;
 import com.googlecode.utterlyidle.Response;
-import com.googlecode.utterlyidle.ResponseBuilder;
 import com.googlecode.utterlyidle.sitemesh.PropertyMap;
 import com.googlecode.utterlyidle.sitemesh.PropertyMapParser;
 
-import java.util.List;
+import java.io.IOException;
 
 import static com.googlecode.funclate.Model.model;
-import static com.googlecode.totallylazy.Sequences.sequence;
-import static com.googlecode.utterlyidle.profiling.FunclateModelRenderer.funclateModelRenderer;
+import static com.googlecode.utterlyidle.Requests.query;
+import static com.googlecode.utterlyidle.ResponseBuilder.modify;
+import static com.googlecode.utterlyidle.profiling.ProfilingClient.profile;
 
 public class ProfilingHandler implements HttpHandler {
     public static final String QUERY_PARAMETER = "profile";
-    private Application application;
-    private HttpHandler httpHandler;
+    private final HttpHandler httpHandler;
+    private final ProfilingData profilingData;
 
-    public ProfilingHandler(final Application application, final HttpHandler httpHandler) {
-        this.application = application;
+    public ProfilingHandler(final HttpHandler httpHandler, final ProfilingData profilingData) {
         this.httpHandler = httpHandler;
+        this.profilingData = profilingData;
     }
 
     @Override
     public Response handle(Request request) throws Exception {
-        QueryParameters parameters = Requests.query(request);
-
-        if (!parameters.contains(QUERY_PARAMETER)) {
+        if (!query(request).contains(QUERY_PARAMETER)) {
             return httpHandler.handle(request);
         }
 
-        StatsCollector stats = StatsCollector.begin();
-        try {
-            Parameters noProfile = parameters.remove(QUERY_PARAMETER).remove("decorator");
-            Uri noProfileUri = request.uri().query(noProfile.toString().replace("?", ""));
-            Request newRequest = Requests.request(request.method(), noProfileUri, request.headers(), request.entity());
-
-            Response response = application.handle(newRequest);
-
-            PropertyMap html = new PropertyMapParser().parse(response.entity().toString());
-
-            List<Triple<Request, Response, Long>> pairs = stats.executionTimes();
-            return render(Model.model().add("executionTimes", sequence(pairs).map(asModel()).toList()).
-                    add("response", html));
-        } finally {
-            StatsCollector.end();
-        }
+        return decorate(profile(httpHandler, profilingData).handle(request));
     }
 
-    private Response render(Model model) throws Exception {
-        return ResponseBuilder.response().entity(funclateModelRenderer(ProfilingHandler.class).render(model).getBytes()).build();
-    }
-
-    private Callable1<Triple<Request, Response, Long>, Model> asModel() {
-        return new Callable1<Triple<Request, Response, Long>, Model>() {
-            @Override
-            public Model call(Triple<Request, Response, Long> triple) throws Exception {
-                return model().add("path", triple.first().uri()).
-                        add("statusCode", triple.second().status()).
-                        add("executionTime", triple.third());
-            }
-        };
+    private Response decorate(Response response) throws IOException {
+        PropertyMap html = new PropertyMapParser().parse(response.entity().toString());
+        EnhancedStringTemplateGroup group = new EnhancedStringTemplateGroup(getClass());
+        return modify(response).entity(group.getInstanceOf("profile", model().
+                add("response", html).
+                add("executionTimes", profilingData.data()).toMap()).toString()).build();
     }
 }
