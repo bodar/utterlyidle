a4229af15e24f6b2ea2b76b84b6c51e02da1258b
dan <dan@bodar.com>
2012-03-23 16:18:59 +0000

    Added logging for records

--- src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
@@ -1,5 +1,6 @@
 package com.googlecode.utterlyidle.profiling;
 
+import com.googlecode.lazyrecords.Loggers;
 import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.handlers.HttpClient;
 import com.googlecode.utterlyidle.modules.Module;
@@ -9,9 +10,13 @@ import com.googlecode.yadic.Container;
 public class ProfilingModule implements RequestScopedModule {
     @Override
     public Module addPerRequestObjects(Container container) throws Exception {
+        ProfilingData data = new ProfilingData();
+        if (container.contains(Loggers.class)) {
+            container.get(Loggers.class).add(data);
+        }
         container.decorate(HttpHandler.class, ProfilingHandler.class);
         container.decorate(HttpClient.class, ProfilingClient.class);
-        container.add(ProfilingData.class);
+        container.addInstance(ProfilingData.class, data);
         return this;
     }
 }
