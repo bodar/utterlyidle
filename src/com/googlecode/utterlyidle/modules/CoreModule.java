329dbc781be999b16f8717ade95dbfa4b87092cc
raymond and tom <raymond and tom@localhost>
2012-05-22 13:41:33 +0100

    Added support for list of UUIDs to argument scope

--- src/com/googlecode/utterlyidle/modules/CoreModule.java
+++ src/com/googlecode/utterlyidle/modules/CoreModule.java
@@ -1,35 +1,36 @@
 package com.googlecode.utterlyidle.modules;
 
+import com.googlecode.totallylazy.Callable1;
 import com.googlecode.totallylazy.Either;
 import com.googlecode.totallylazy.Option;
 import com.googlecode.totallylazy.Predicate;
-import com.googlecode.totallylazy.Strings;
-import com.googlecode.totallylazy.Uri;
-import com.googlecode.totallylazy.predicates.LogicalPredicate;
 import com.googlecode.totallylazy.time.Clock;
 import com.googlecode.totallylazy.time.SystemClock;
 import com.googlecode.utterlyidle.*;
 import com.googlecode.utterlyidle.cookies.CookieParameters;
 import com.googlecode.utterlyidle.handlers.*;
-import com.googlecode.utterlyidle.io.HierarchicalPath;
 import com.googlecode.utterlyidle.rendering.ExceptionRenderer;
 import com.googlecode.utterlyidle.rendering.MatchFailureRenderer;
 import com.googlecode.utterlyidle.rendering.ObjectRenderer;
 import com.googlecode.yadic.Container;
+import com.googlecode.yadic.Resolver;
 import com.googlecode.yadic.generics.TypeFor;
 import com.googlecode.yadic.resolvers.OptionResolver;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.util.UUID;
+import java.util.concurrent.Callable;
 
 import static com.googlecode.totallylazy.Predicates.*;
+import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.URLs.packageUrl;
+import static com.googlecode.totallylazy.Unchecked.cast;
 import static com.googlecode.utterlyidle.dsl.DslBindings.bindings;
 import static com.googlecode.utterlyidle.dsl.StaticBindingBuilder.in;
 import static com.googlecode.utterlyidle.handlers.HandlerRule.entity;
 import static com.googlecode.utterlyidle.handlers.RenderingResponseHandler.renderer;
-import static com.googlecode.utterlyidle.io.HierarchicalPath.hierarchicalPath;
 
 public class CoreModule extends AbstractModule {
     @Override
@@ -96,8 +97,9 @@ public class CoreModule extends AbstractModule {
         argumentScope.addInstance(Entity.class, request.entity());
         argumentScope.addInstance(InputStream.class, new ByteArrayInputStream(request.entity().asBytes()));
         argumentScope.addType(new TypeFor<Option<?>>() {}.get(), new OptionResolver(argumentScope, instanceOf(IllegalArgumentException.class)));
-        argumentScope.addType(new TypeFor<Either<?, ?>>() {}.get(), new EitherResolver(argumentScope));
+        argumentScope.addType(new TypeFor<Either<?, ?>>() {}.get(), EitherResolver.class);
         argumentScope.addActivator(UUID.class, UUIDActivator.class);
+        argumentScope.addType(new TypeFor<Iterable<UUID>>() {}.get(), new TypedIterableResolver<UUID>(argumentScope, UUIDActivator.fromString()));
         return this;
     }
 
@@ -109,4 +111,22 @@ public class CoreModule extends AbstractModule {
             }
         };
     }
+
+    public static class TypedIterableResolver<T> implements Resolver<Iterable<T>> {
+        private final Container container;
+        private final Callable1<String, T> mapper;
+
+        public TypedIterableResolver(Container container, Callable1<String, T> mapper) {
+            this.container = container;
+            this.mapper = mapper;
+        }
+
+        @Override
+        public Iterable<T> resolve(Type type) throws Exception {
+            Iterable<String> values = cast(container.resolve(new TypeFor<Iterable<String>>() {}.get()));
+            return sequence(values).map(mapper);
+        }
+
+    }
+
 }
