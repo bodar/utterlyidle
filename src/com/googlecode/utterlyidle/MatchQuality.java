643b60220a3f81508db64d3bac1814d8d923ddc7
Daniel Worthington-Bodart <dan@bodar.com>
2012-05-24 12:00:16 +0100

    Breaking change: Resource path length matching now takes priority

--- src/com/googlecode/utterlyidle/MatchQuality.java
+++ src/com/googlecode/utterlyidle/MatchQuality.java
@@ -1,21 +1,27 @@
 package com.googlecode.utterlyidle;
 
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Function1;
+
 import java.util.Comparator;
 
 import static com.googlecode.totallylazy.comparators.Comparators.comparators;
 import static com.googlecode.totallylazy.comparators.Comparators.descending;
 import static com.googlecode.totallylazy.comparators.Comparators.where;
 import static com.googlecode.utterlyidle.Binding.functions.numberOfArguments;
+import static com.googlecode.utterlyidle.Binding.functions.pathSegments;
 import static com.googlecode.utterlyidle.Binding.functions.priority;
 import static com.googlecode.utterlyidle.ProducesMimeMatcher.quality;
 
 public class MatchQuality {
     public static Comparator<Binding> matchQuality(Request request) {
         return comparators(
+                where(pathSegments(), descending(Integer.class)),
                 where(quality(request), descending(Float.class)),
                 where(priority(), descending(Integer.class)),
                 where(numberOfArguments(), descending(Integer.class))
         );
     }
+
 
 }
