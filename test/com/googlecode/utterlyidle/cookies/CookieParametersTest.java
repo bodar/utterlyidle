a7695f40601d5487dc7efba4b2dedbbf3d9d6823
raymond <raymond@localhost>
2012-08-09 15:25:36 +0100

    Fixed bug in cookie string encode/decode

--- test/com/googlecode/utterlyidle/cookies/CookieParametersTest.java
+++ test/com/googlecode/utterlyidle/cookies/CookieParametersTest.java
@@ -8,7 +8,10 @@ import com.googlecode.utterlyidle.annotations.HttpMethod;
 import org.junit.Test;
 
 import static com.googlecode.totallylazy.Pair.pair;
+import static com.googlecode.totallylazy.Sequences.one;
+import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.utterlyidle.HeaderParameters.headerParameters;
+import static com.googlecode.utterlyidle.RequestBuilder.get;
 import static com.googlecode.utterlyidle.cookies.Cookie.cookie;
 import static com.googlecode.utterlyidle.cookies.CookieParameters.cookies;
 import static com.googlecode.utterlyidle.cookies.CookieParameters.toHttpHeader;
@@ -24,15 +27,15 @@ public class CookieParametersTest extends ParametersContract<CookieParameters> {
 
     @Test
     public void shouldHandleTrailingSpaces() throws Exception {
-        CookieParameters cookies = cookies(request(headerParameters(pair("Cookie", "a=1; ; ;"))).headers());
+        CookieParameters cookies = cookies(request(headerParameters(one(pair("Cookie", "a=1; ; ;")))).headers());
 
         assertThat(cookies.getValue("a"), is("1"));
     }
 
     @Test
     public void shouldBeCaseInsensitive() throws Exception {
-        CookieParameters lowercaseCookies = cookies(request(headerParameters(pair("cookie", "a=1"))).headers());
-        CookieParameters uppercaseCookies = cookies(request(headerParameters(pair("COOKIE", "b=2"))).headers());
+        CookieParameters lowercaseCookies = cookies(request(headerParameters(one(pair("cookie", "a=1")))).headers());
+        CookieParameters uppercaseCookies = cookies(request(headerParameters(one(pair("COOKIE", "b=2")))).headers());
 
         assertThat(lowercaseCookies.getValue("a"), is("1"));
         assertThat(uppercaseCookies.getValue("b"), is("2"));
@@ -40,7 +43,7 @@ public class CookieParametersTest extends ParametersContract<CookieParameters> {
 
     @Test
     public void shouldCopeWithRequestCookiesInMultipleHeaders() throws Exception {
-        CookieParameters cookies = cookies(request(headerParameters(pair("Cookie", "a=1"), pair("Cookie", "b=2"))).headers());
+        CookieParameters cookies = cookies(request(headerParameters(sequence(pair("Cookie", "a=1"), pair("Cookie", "b=2")))).headers());
 
         assertThat(cookies.getValue("a"), is("1"));
         assertThat(cookies.getValue("b"), is("2"));
@@ -48,7 +51,7 @@ public class CookieParametersTest extends ParametersContract<CookieParameters> {
 
     @Test
     public void willIgnoreAttributesOnRequestCookiesForTheTimeBeing() throws Exception {
-        CookieParameters cookies = cookies(request(headerParameters(pair("Cookie", "$Version=1; a=1; $Path=whatever; $Domain=something; b=2"))).headers());
+        CookieParameters cookies = cookies(request(headerParameters(one(pair("Cookie", "$Version=1; a=1; $Path=whatever; $Domain=something; b=2")))).headers());
 
         assertThat(cookies.getValue("$Version"), is(nullValue()));
         assertThat(cookies.getValue("$Path"), is(nullValue()));
@@ -63,17 +66,15 @@ public class CookieParametersTest extends ParametersContract<CookieParameters> {
         assertThat(
                 toHttpHeader("a", cookie("Some \"double quoted thing\"")),
                 is("a=\"Some \\\"double quoted thing\\\"\"; "));
+
+        Request request = get("").cookie("a", Cookie.cookie("\"")).build();
+        assertThat(cookies(request.headers()).getValue("a"), is("\""));
     }
 
     @Test
-    public void copesWithCookieHeaderWithNoCookiesBoo() {
-        CookieParameters cookies = cookies(request(headerParameters(pair("Cookie", ""))).headers());
-
+    public void copesWithCookieHeaderWithNoCookies() {
+        CookieParameters cookies = cookies(request(headerParameters(one(pair("Cookie", "")))).headers());
         assertThat(cookies.size(), is(0));
-    }
-
-    private Request someRequest() {
-        return request(headerParameters());
     }
 
     private Request request(HeaderParameters headers) {
