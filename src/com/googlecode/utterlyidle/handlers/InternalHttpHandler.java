3d6e295276ed2457e0417ab2e11229db672ef933
stuart & raymond <spmiller@users.noreply.github.com>
2012-08-21 11:58:13 +0100

    added header whitelists to the InternalHttpHandler

--- src/com/googlecode/utterlyidle/handlers/InternalHttpHandler.java
+++ src/com/googlecode/utterlyidle/handlers/InternalHttpHandler.java
@@ -1,23 +1,47 @@
 package com.googlecode.utterlyidle.handlers;
 
+import com.googlecode.totallylazy.Callable2;
+import com.googlecode.totallylazy.Callables;
+import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.Sequence;
 import com.googlecode.utterlyidle.Application;
 import com.googlecode.utterlyidle.HttpHandler;
-import com.googlecode.utterlyidle.HttpHeaders;
 import com.googlecode.utterlyidle.InternalRequestMarker;
 import com.googlecode.utterlyidle.Request;
+import com.googlecode.utterlyidle.RequestBuilder;
 import com.googlecode.utterlyidle.Response;
 
+import static com.googlecode.totallylazy.Predicates.in;
+import static com.googlecode.totallylazy.Predicates.where;
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.utterlyidle.HttpHeaders.COOKIE;
+import static com.googlecode.utterlyidle.RequestBuilder.modify;
+
 public class InternalHttpHandler implements HttpHandler {
+    private static final Sequence<String> HEADER_WHITE_LIST = sequence(COOKIE);
+
     private final InternalRequestMarker marker;
     private final Application application;
+    private final Request originalRequest;
 
-    public InternalHttpHandler(InternalRequestMarker marker, Application application) {
+    public InternalHttpHandler(InternalRequestMarker marker, Application application, Request originalRequest) {
         this.marker = marker;
         this.application = application;
+        this.originalRequest = originalRequest;
     }
 
     @Override
     public Response handle(Request request) throws Exception {
-        return application.handle(marker.markAsInternal(request));
+        Request requestWithHeaders = sequence(originalRequest.headers()).filter(where(Callables.<String>first(), in(HEADER_WHITE_LIST))).fold(modify(request), addHeader()).build();
+        return application.handle(marker.markAsInternal(requestWithHeaders));
+    }
+
+    private Callable2<RequestBuilder, Pair<String, String>, RequestBuilder> addHeader() {
+        return new Callable2<RequestBuilder, Pair<String, String>, RequestBuilder>() {
+            @Override
+            public RequestBuilder call(RequestBuilder requestBuilder, Pair<String, String> headerPair) throws Exception {
+                return requestBuilder.header(headerPair.first(), headerPair.second());
+            }
+        };
     }
 }
