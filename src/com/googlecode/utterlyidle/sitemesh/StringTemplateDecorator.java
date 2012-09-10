0f8fa0317057e1c878983d2ea672b63bde624215
spmiller <spmiller@gmail.com>
2012-09-10 16:39:22 +0100

    Add support for a footer id section, which needs more tidyup

--- src/com/googlecode/utterlyidle/sitemesh/StringTemplateDecorator.java
+++ src/com/googlecode/utterlyidle/sitemesh/StringTemplateDecorator.java
@@ -1,7 +1,11 @@
 package com.googlecode.utterlyidle.sitemesh;
 
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Callables;
 import com.googlecode.totallylazy.LazyException;
 import com.googlecode.totallylazy.Maps;
+import com.googlecode.totallylazy.Option;
+import com.googlecode.totallylazy.Strings;
 import com.googlecode.utterlyidle.BasePath;
 import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.QueryParameters;
@@ -10,10 +14,16 @@ import com.googlecode.utterlyidle.Requests;
 import com.googlecode.utterlyidle.handlers.HttpClient;
 import org.antlr.stringtemplate.NoIndentWriter;
 import org.antlr.stringtemplate.StringTemplate;
+import org.sitemesh.content.Content;
+import org.sitemesh.content.ContentProcessor;
+import org.sitemesh.content.ContentProperty;
+import org.sitemesh.content.tagrules.TagBasedContentProcessor;
+import org.sitemesh.content.tagrules.html.DivExtractingTagRuleBundle;
 
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.nio.CharBuffer;
 
 public class StringTemplateDecorator implements Decorator {
     private final StringTemplate template;
@@ -40,8 +50,29 @@ public class StringTemplateDecorator implements Decorator {
         template.setAttribute("meta", content.getPropertyMap("meta"));
         template.setAttribute("head", content.get("head"));
         template.setAttribute("title", content.get("title"));
-        template.setAttribute("body", content.get("body"));
+        String footer = findFooter(content.get("body").toString());
+        template.setAttribute("footer", footer);
+        template.setAttribute("body", content.get("body").toString().replace(Strings.asString(footer), "").replace("<div id=\"footer\"></div>", ""));
         return this;
+    }
+
+    public String findFooter(String buffer) throws IOException {
+        ContentProcessor contentProcessor = new TagBasedContentProcessor(new DivExtractingTagRuleBundle());
+        Content content = contentProcessor.build(CharBuffer.wrap(buffer), null);
+        ContentProperty extractedProperties = content.getExtractedProperties();
+        return Option.option(extractedProperties).map(getChild("div")).map(getChild("footer")).map(Callables.asString()).getOrNull();
+    }
+
+    private Callable1<? super ContentProperty, ContentProperty> getChild(final String childName) {
+        return new Callable1<ContentProperty, ContentProperty>() {
+            @Override
+            public ContentProperty call(ContentProperty contentProperty) throws Exception {
+                if (!contentProperty.hasChild(childName)) {
+                    return null;
+                }
+                return contentProperty.getChild(childName);
+            }
+        };
     }
 
     public String decorate(String content) throws IOException {
