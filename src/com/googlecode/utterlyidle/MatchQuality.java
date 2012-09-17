fa47a736cbe256598853e64b96e88bc069e24dc4
spmiller <spmiller@users.noreply.github.com>
2012-09-17 11:07:24 +0100

    Default arguments should rank less than named arguments when the number of arguments is the same

--- src/com/googlecode/utterlyidle/MatchQuality.java
+++ src/com/googlecode/utterlyidle/MatchQuality.java
@@ -2,10 +2,12 @@ package com.googlecode.utterlyidle;
 
 import java.util.Comparator;
 
+import static com.googlecode.totallylazy.comparators.Comparators.ascending;
 import static com.googlecode.totallylazy.comparators.Comparators.comparators;
 import static com.googlecode.totallylazy.comparators.Comparators.descending;
 import static com.googlecode.totallylazy.comparators.Comparators.where;
 import static com.googlecode.utterlyidle.Binding.functions.numberOfArguments;
+import static com.googlecode.utterlyidle.Binding.functions.numberOfDefaultArguments;
 import static com.googlecode.utterlyidle.Binding.functions.pathSegments;
 import static com.googlecode.utterlyidle.Binding.functions.priority;
 import static com.googlecode.utterlyidle.ProducesMimeMatcher.quality;
@@ -16,7 +18,8 @@ public class MatchQuality {
                 where(pathSegments(), descending(Integer.class)),
                 where(quality(request), descending(Float.class)),
                 where(priority(), descending(Integer.class)),
-                where(numberOfArguments(), descending(Integer.class))
+                where(numberOfArguments(), descending(Integer.class)),
+                where(numberOfDefaultArguments(), ascending(Integer.class))
         );
     }
 }
