8b87b54fb4b2e3dee4609f4c992e1f883364ba4f
Raymond Barlow <rbarlow@raymanoz.com>
2012-11-01 14:50:16 +0000

    Replaced StubHandler with RecordingHandler

--- test/com/googlecode/utterlyidle/handlers/ConvertExtensionToAcceptHeaderTest.java
+++ test/com/googlecode/utterlyidle/handlers/ConvertExtensionToAcceptHeaderTest.java
@@ -2,6 +2,7 @@ package com.googlecode.utterlyidle.handlers;
 
 import com.googlecode.utterlyidle.HttpHeaders;
 import com.googlecode.utterlyidle.Request;
+import com.googlecode.utterlyidle.Response;
 import org.junit.Test;
 
 import static com.googlecode.totallylazy.Pair.pair;
@@ -9,15 +10,16 @@ import static com.googlecode.totallylazy.Uri.uri;
 import static com.googlecode.utterlyidle.HttpHeaders.LOCATION;
 import static com.googlecode.utterlyidle.RequestBuilder.get;
 import static com.googlecode.utterlyidle.Response.methods.header;
+import static com.googlecode.utterlyidle.ResponseBuilder.response;
 import static com.googlecode.utterlyidle.Responses.seeOther;
 import static com.googlecode.utterlyidle.handlers.ConvertExtensionToAcceptHeader.Replacements.replacements;
+import static com.googlecode.utterlyidle.handlers.RecordingHttpHandler.recordingHttpHandler;
+import static com.googlecode.utterlyidle.handlers.ReturnResponseHandler.returnsResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
 public class ConvertExtensionToAcceptHeaderTest {
-    private StubHttpHandler httpHandler = new StubHttpHandler();
-
     @Test
     public void findsExtensionInRequestUrl() {
         assertThat(extensionOf("/spong"), is(nullValue()));
@@ -33,64 +35,71 @@ public class ConvertExtensionToAcceptHeaderTest {
 
     @Test
     public void removesExtensionFromRequestUrl() throws Exception {
-        ConvertExtensionToAcceptHeader converter = new ConvertExtensionToAcceptHeader(
-                replacements(pair("properties", "text/plain"), pair("html", "text/html")),
-                httpHandler);
+        assertUrlConversion("/resource.properties?twigs=berries", "/resource?twigs=berries");
+        assertUrlConversion("/resource.html?twigs=berries", "/resource?twigs=berries");
+        assertUrlConversion("/.html?twigs=berries", "/?twigs=berries");
 
-        assertUrlConversion(converter, "/resource.properties?twigs=berries", "/resource?twigs=berries");
-        assertUrlConversion(converter, "/resource.html?twigs=berries", "/resource?twigs=berries");
-        assertUrlConversion(converter, "/.html?twigs=berries", "/?twigs=berries");
-
-        assertUrlConversion(converter,
+        assertUrlConversion(
                 "/something.html?getUrl=/something_else.xml",
                 "/something?getUrl=/something_else.xml");
 
-        assertUrlConversion(converter, "/resource.notmapped?twigs=berries", "/resource.notmapped?twigs=berries");
+        assertUrlConversion("/resource.notmapped?twigs=berries", "/resource.notmapped?twigs=berries");
     }
 
     @Test
     public void replacesAcceptHeaderWithImpliedMediaType() throws Exception {
-        ConvertExtensionToAcceptHeader converter = new ConvertExtensionToAcceptHeader(
-                replacements(pair("properties", "text/plain"), pair("html", "text/html")),
-                httpHandler);
-
-        assertHeaderConversion(converter, "resource.properties?twigs=berries", "text/plain");
-        assertHeaderConversion(converter, "resource.html?twigs=berries", "text/html");
+        assertHeaderConversion("resource.properties?twigs=berries", "text/plain");
+        assertHeaderConversion("resource.html?twigs=berries", "text/html");
     }
 
     @Test
     public void addsExtensionBackInToLocationHeaderForRedirects() throws Exception {
-        ConvertExtensionToAcceptHeader converter = new ConvertExtensionToAcceptHeader(
-                replacements(pair("properties", "text/plain"), pair("html", "text/html")),
-                httpHandler);
-
-        httpHandler.respondsWith(seeOther("/redirect/to/here"));
+        ConvertExtensionToAcceptHeader converter = converter(respondsWith(seeOther("/redirect/to/here")));
         assertThat(
                 header(converter.handle(get("anything.properties").build()), LOCATION),
                 is("/redirect/to/here.properties"));
 
-        httpHandler.respondsWith(seeOther("/redirect/to/here.moo?queryparams=some"));
+        converter = converter(respondsWith(seeOther("/redirect/to/here.moo?queryparams=some")));
         assertThat(
                 header(converter.handle(get("anything.properties").build()), LOCATION),
                 is("/redirect/to/here.moo.properties?queryparams=some"));
     }
 
-    private void assertHeaderConversion(ConvertExtensionToAcceptHeader converter, String url, String expectedMimeType) throws Exception {
+    private void assertHeaderConversion(String url, String expectedMimeType) throws Exception {
+        RecordingHttpHandler httpHandler = respondsWith(response().build());
+
+        ConvertExtensionToAcceptHeader converter = converter(httpHandler);
+
         Request request = get(url).build();
         converter.handle(request);
 
-        assertThat(httpHandler.request.headers().getValue(HttpHeaders.ACCEPT), is(expectedMimeType));
+        assertThat(httpHandler.requests().head().headers().getValue(HttpHeaders.ACCEPT), is(expectedMimeType));
     }
 
-    private void assertUrlConversion(ConvertExtensionToAcceptHeader converter, String original, String expectedAfterConversion) throws Exception {
+    private ConvertExtensionToAcceptHeader converter(RecordingHttpHandler httpHandler) {
+        return new ConvertExtensionToAcceptHeader(
+                replacements(pair("properties", "text/plain"), pair("html", "text/html")),
+                httpHandler);
+    }
+
+    private RecordingHttpHandler respondsWith(Response response) {
+        return recordingHttpHandler(returnsResponse(response));
+    }
+
+    private void assertUrlConversion(String original, String expectedAfterConversion) throws Exception {
+        RecordingHttpHandler httpHandler = respondsWith(response().build());
+
+        ConvertExtensionToAcceptHeader converter = converter(httpHandler);
+
         Request request = get(original).build();
         converter.handle(request);
 
-        assertThat(httpHandler.request.uri(), is(uri(expectedAfterConversion)));
+        assertThat(httpHandler.requests().head().uri(), is(uri(expectedAfterConversion)));
     }
+
+
 
     private String extensionOf(String url) {
         return ConvertExtensionToAcceptHeader.fileExtension(uri(url)).getOrNull();
     }
-
 }
--- test/com/googlecode/utterlyidle/handlers/HeadRequestHandlerTest.java
+++ test/com/googlecode/utterlyidle/handlers/HeadRequestHandlerTest.java
@@ -12,6 +12,7 @@ import static com.googlecode.utterlyidle.Status.NOT_FOUND;
 import static com.googlecode.utterlyidle.Status.OK;
 import static com.googlecode.utterlyidle.annotations.HttpMethod.GET;
 import static com.googlecode.utterlyidle.annotations.HttpMethod.HEAD;
+import static com.googlecode.utterlyidle.handlers.ReturnResponseHandler.returnsResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
@@ -21,14 +22,16 @@ public class HeadRequestHandlerTest {
     public static final String TARGET = "whatever";
     public static final Status NOT_SUCCESSFUL_STATUS = NOT_FOUND;
 
-    StubHttpHandler stubHttpHandler = new StubHttpHandler();
-    HeadRequestHandler headHandler = new HeadRequestHandler(stubHttpHandler);
     Request HEAD_REQUEST = new RequestBuilder(HEAD, TARGET).build();
     Request NOT_HEAD_REQUEST = new RequestBuilder(GET, TARGET).build();
 
+    private HeadRequestHandler respondsWith(Response response) {
+        return new HeadRequestHandler(RecordingHttpHandler.recordingHttpHandler(returnsResponse(response)));
+    }
+
     @Test
     public void shouldReturnResponseWithNoEntityWhenTheResponseStatusIsOk() throws Exception {
-        stubHttpHandler.respondsWith(response(OK).entity(ENTITY).build());
+        HeadRequestHandler headHandler = respondsWith(response(OK).entity(ENTITY).build());
 
         Response response = headHandler.handle(HEAD_REQUEST);
 
@@ -38,7 +41,7 @@ public class HeadRequestHandlerTest {
 
     @Test
     public void shouldReturnResponseWithNoEntityWhenTheResponseStatusIsOtherThanOk() throws Exception {
-        stubHttpHandler.respondsWith(response(NOT_SUCCESSFUL_STATUS).entity(ENTITY).build());
+        HeadRequestHandler headHandler = respondsWith(response(NOT_SUCCESSFUL_STATUS).entity(ENTITY).build());
 
         Response response = headHandler.handle(HEAD_REQUEST);
 
@@ -48,12 +51,11 @@ public class HeadRequestHandlerTest {
 
     @Test
     public void shouldReturnResponseWithEntityIfTheRequestMethodOtherThanHead() throws Exception {
-        stubHttpHandler.respondsWith(response(OK).entity(ENTITY).build());
+        HeadRequestHandler headHandler = respondsWith(response(OK).entity(ENTITY).build());
 
         Response response = headHandler.handle(NOT_HEAD_REQUEST);
 
         assertThat(response.status(), is(OK));
         assertThat(response.entity(), is(not(empty())));
     }
-
 }
