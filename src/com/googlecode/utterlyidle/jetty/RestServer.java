696c1d4cf6a860ba3e70601cf5f11cdd6cd00098
Daniel Worthington-Bodart <dan@bodar.com>
2013-02-14 15:54:16 +0000

    added webXmlRestServer() factory method to jetty RestServer

--- src/com/googlecode/utterlyidle/jetty/RestServer.java
+++ src/com/googlecode/utterlyidle/jetty/RestServer.java
@@ -1,5 +1,6 @@
 package com.googlecode.utterlyidle.jetty;
 
+import com.googlecode.totallylazy.Function1;
 import com.googlecode.totallylazy.Uri;
 import com.googlecode.utterlyidle.Application;
 import com.googlecode.utterlyidle.ServerConfiguration;
@@ -10,6 +11,7 @@ import com.googlecode.utterlyidle.servlet.ServletModule;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.bio.SocketConnector;
 import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.webapp.WebAppContext;
 import org.mortbay.thread.QueuedThreadPool;
 
 import java.io.IOException;
@@ -23,11 +25,25 @@ import static java.lang.System.nanoTime;
 import static org.mortbay.jetty.servlet.Context.NO_SESSIONS;
 
 public class RestServer implements com.googlecode.utterlyidle.Server {
-    private final Server server;
+    private final Application application;
+    private final ServerConfiguration configuration;
+    private Server server;
     private Uri uri;
+    private final Function1<Server, Context> createContext;
 
-    public RestServer(final Application application, final ServerConfiguration configuration) throws Exception {
-        server = startApp(application, configuration);
+    private RestServer(final Application application, final ServerConfiguration configuration, Function1<Server, Context> contextCreator) throws Exception {
+        this.application = application;
+        this.configuration = configuration;
+        this.createContext = contextCreator;
+        server = startApp();
+    }
+
+    public static RestServer restServer(final Application application, final ServerConfiguration configuration) throws Exception {
+        return new RestServer(application, configuration, defaultContext(application, configuration));
+    }
+
+    public static RestServer webXmlRestServer(final Application application, final ServerConfiguration configuration, Uri webRoot) throws Exception {
+        return new RestServer(application, configuration, webXmlContext(webRoot, configuration));
     }
 
     public void close() throws IOException {
@@ -42,23 +58,44 @@ public class RestServer implements com.googlecode.utterlyidle.Server {
         application(HelloWorldApplication.class).start(defaultConfiguration().port(8002));
     }
 
-    private Server startApp(Application application, final ServerConfiguration serverConfig) throws Exception {
+    private Server startApp() throws Exception {
         long start = nanoTime();
-        Server server = startUpServer(application, serverConfig);
+        Server server = startUpServer();
         System.out.println(format("Listening on %s, started Jetty in %s msecs", uri, calculateMilliseconds(start, nanoTime())));
         Service.functions.start().callConcurrently(application);
         return server;
     }
 
-    private Server startUpServer(Application application, ServerConfiguration serverConfig) throws Exception {
-        Server server = createServer(serverConfig);
-        Context context = new Context(server, serverConfig.basePath().toString(), NO_SESSIONS);
-        application.add(new ServletModule(context.getServletContext()));
+    private Server startUpServer() throws Exception {
+        Server server = createServer(configuration);
+
+        Context context = createContext.call(server);
         context.setAttribute(Application.class.getCanonicalName(), application);
-        context.addServlet(ApplicationServlet.class, "/*");
+
         server.start();
-        uri = serverConfig.port(getPortNumber(server)).toUrl();
+        uri = configuration.port(getPortNumber(server)).toUrl();
         return server;
+    }
+
+    public static Function1<Server, Context> defaultContext(final Application application, final ServerConfiguration configuration) {
+        return new Function1<Server, Context>() {
+            @Override
+            public Context call(Server server) throws Exception {
+                Context context = new Context(server, configuration.basePath().toString(), NO_SESSIONS);
+                application.add(new ServletModule(context.getServletContext()));
+                context.addServlet(ApplicationServlet.class, "/*");
+                return context;
+            }
+        };
+    }
+
+    public static Function1<Server, Context> webXmlContext(final Uri webRoot, final ServerConfiguration configuration) {
+        return new Function1<Server, Context>() {
+            @Override
+            public Context call(Server server) throws Exception {
+                return new WebAppContext(server, webRoot.toString(), configuration.basePath().toString());
+            }
+        };
     }
 
     private Server createServer(ServerConfiguration serverConfig) {
