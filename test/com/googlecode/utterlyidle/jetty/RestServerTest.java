7d496842eaa639abee07ffec2e3f206be2c1824a
Daniel Worthington-Bodart <dan@bodar.com>
2013-03-04 10:08:03 +0000

    Swapping Jetty application instance

--- test/com/googlecode/utterlyidle/jetty/RestServerTest.java
+++ test/com/googlecode/utterlyidle/jetty/RestServerTest.java
@@ -1,11 +1,31 @@
 package com.googlecode.utterlyidle.jetty;
 
+import com.googlecode.utterlyidle.Application;
+import com.googlecode.utterlyidle.BasePath;
 import com.googlecode.utterlyidle.Server;
 import com.googlecode.utterlyidle.ServerContract;
+import com.googlecode.utterlyidle.examples.HelloWorldApplication;
+import com.googlecode.utterlyidle.servlet.ApplicationContext;
+import org.junit.Test;
 
-public class RestServerTest extends ServerContract {
+import javax.servlet.ServletContext;
+import java.applet.AppletContext;
+
+import static com.googlecode.totallylazy.matchers.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.sameInstance;
+
+public class RestServerTest extends ServerContract<RestServer> {
     @Override
-    protected Class<? extends Server> server() throws Exception {
+    protected Class<RestServer> server() throws Exception {
         return RestServer.class;
+    }
+
+    @Test
+    public void canSwapApplicationAfterStart() throws Exception {
+        ServletContext context = server.servletContext();
+        Application newApp = new HelloWorldApplication(BasePath.basePath("foo"));
+        ApplicationContext.setApplication(context, newApp);
+        assertThat(newApp, sameInstance(ApplicationContext.getApplication(context, null)));
     }
 }
