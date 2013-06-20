b5db2d4f8fe95ed0008dbc1ceb13a8a7658b21d4
Daniel Worthington-Bodart <dan@bodar.com>
2013-06-20 10:07:50 +0100

    Updated TL to 1090

--- src/com/googlecode/utterlyidle/BindingsRequestGenerator.java
+++ src/com/googlecode/utterlyidle/BindingsRequestGenerator.java
@@ -1,6 +1,9 @@
 package com.googlecode.utterlyidle;
 
+import com.googlecode.totallylazy.annotations.multimethod;
+import com.googlecode.totallylazy.multi;
 import com.googlecode.totallylazy.proxy.Invocation;
+import com.googlecode.totallylazy.proxy.MethodInvocation;
 import com.googlecode.utterlyidle.annotations.HttpMethod;
 
 import static com.googlecode.utterlyidle.Extractors.extractForm;
@@ -16,13 +19,18 @@ public class BindingsRequestGenerator implements RequestGenerator {
 
     @Override
     public Request requestFor(final Invocation invocation) {
+        return new multi() {}.method(invocation);
+    }
+
+    @multimethod
+    public Request requestFor(final MethodInvocation invocation) {
         return requestFor(bindings.find(invocation.method()).get(), invocation.arguments());
     }
 
     @Override
     public Request requestFor(final Binding binding, final Object... arguments) {
         RequestBuilder requestBuilder = new RequestBuilder(binding.httpMethod(), redirector.uriOf(binding, arguments));
-        if(binding.httpMethod().equals(HttpMethod.POST)) {
+        if (binding.httpMethod().equals(HttpMethod.POST)) {
             requestBuilder.forms(extractForm(binding, arguments));
         }
         return requestBuilder.build();
