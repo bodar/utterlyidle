ec28b85fdf11707517ae4280e8da5dff40a87008
dan and jorge <dan and jorge@localhost>
2013-06-25 10:54:19 +0100

    content_length header will now be added for non streaming entities

--- test/com/googlecode/utterlyidle/RequestBuilderTest.java
+++ test/com/googlecode/utterlyidle/RequestBuilderTest.java
@@ -5,19 +5,32 @@ import com.googlecode.utterlyidle.cookies.Cookie;
 import com.googlecode.utterlyidle.cookies.CookieParameters;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+
 import static com.googlecode.totallylazy.Pair.pair;
 import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.totallylazy.Strings.bytes;
 import static com.googlecode.utterlyidle.HeaderParameters.headerParameters;
+import static com.googlecode.utterlyidle.HttpHeaders.CONTENT_LENGTH;
 import static com.googlecode.utterlyidle.HttpHeaders.COOKIE;
 import static com.googlecode.utterlyidle.Requests.form;
 import static com.googlecode.utterlyidle.Requests.query;
 import static com.googlecode.utterlyidle.cookies.Cookie.cookie;
 import static com.googlecode.utterlyidle.cookies.CookieParameters.toHttpHeader;
+import static java.lang.String.valueOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RequestBuilderTest {
+    @Test
+    public void alwaysSetsContentLengthForNonStreamingEntity() throws Exception {
+        assertThat(RequestBuilder.get("/home").entity("Hello").build().headers().getValue(CONTENT_LENGTH), equalTo(valueOf(bytes("Hello").length)));
+        assertThat(RequestBuilder.get("/home").entity(bytes("Hello")).build().headers().getValue(CONTENT_LENGTH), equalTo(valueOf(bytes("Hello").length)));
+        assertThat(RequestBuilder.get("/home").entity(new ByteArrayInputStream(bytes("Hello"))).build().headers().contains(CONTENT_LENGTH), equalTo(false));
+        assertThat(RequestBuilder.get("/home").build().headers().contains(CONTENT_LENGTH), equalTo(false));
+    }
+
     @Test
     public void canCreateRequestBuilderFromRequest() {
         Request originalRequest = new RequestBuilder("GET", "/home").
@@ -61,7 +74,7 @@ public class RequestBuilderTest {
     @Test
     public void replacingACookiePreservesHeaderOrder() throws Exception {
         Request request = RequestBuilder.get("/").header("path", "/").cookie("cookie1", "value1").cookie("cookie2", "value2").replaceCookie("cookie2", "penguin").build();
-        assertThat(request.headers(), is(headerParameters(sequence(pair("path", "/"), pair("Cookie", "cookie1=\"value1\""), pair("Cookie", "cookie2=\"penguin\"")))));
+        assertThat(request.headers(), is(headerParameters(sequence(pair("path", "/"), pair(COOKIE, "cookie1=\"value1\""), pair(COOKIE, "cookie2=\"penguin\"")))));
     }
 
     @Test
@@ -69,7 +82,7 @@ public class RequestBuilderTest {
         Request request = RequestBuilder.get("/").header(COOKIE, "cookie1=\"value1\"; cookie2=\"value2\"").replaceCookie("cookie2", "hobnob").build();
         assertThat(request.headers().toMap().get(COOKIE).get(0), is("cookie1=\"value1\"; cookie2=\"hobnob\""));
     }
-    
+
     @Test
     public void canCopyFormParamsIntoQueryParams() {
         Request postForm = RequestBuilder.post("/?three=3").form("one", "1").form("two", 2).build();
@@ -77,9 +90,9 @@ public class RequestBuilderTest {
         Request modified = RequestBuilder.modify(postForm).copyFormParamsToQuery().build();
 
         QueryParameters queryParameters = query(modified);
-        assertThat(queryParameters.getValue("one"),is(equalTo("1")));
-        assertThat(queryParameters.getValue("two"),is(equalTo("2")));
-        assertThat(queryParameters.getValue("three"),is(equalTo("3")));
+        assertThat(queryParameters.getValue("one"), is(equalTo("1")));
+        assertThat(queryParameters.getValue("two"), is(equalTo("2")));
+        assertThat(queryParameters.getValue("three"), is(equalTo("3")));
         FormParameters formParameters = form(modified);
         assertThat(formParameters.size(), is(2));
     }
