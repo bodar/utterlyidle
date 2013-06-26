f29bcc6f0f2e59a470bdd899037a65b3ffa38203
Daniel Worthington-Bodart <dan@bodar.com>
2013-06-26 14:56:32 +0100

    Added try finally to ensure we close input

--- src/com/googlecode/utterlyidle/dsl/StaticResources.java
+++ src/com/googlecode/utterlyidle/dsl/StaticResources.java
@@ -1,24 +1,16 @@
 package com.googlecode.utterlyidle.dsl;
 
-import com.googlecode.totallylazy.Callable1;
 import com.googlecode.totallylazy.LazyException;
 import com.googlecode.totallylazy.Uri;
 import com.googlecode.utterlyidle.HttpHandler;
-import com.googlecode.utterlyidle.HttpHeaders;
 import com.googlecode.utterlyidle.RequestBuilder;
 import com.googlecode.utterlyidle.Response;
-import com.googlecode.utterlyidle.ResponseBuilder;
 import com.googlecode.utterlyidle.handlers.ClientHttpHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 
-import static com.googlecode.totallylazy.Bytes.bytes;
-import static com.googlecode.totallylazy.Closeables.using;
-
 public class StaticResources {
-    public Response get(URL baseUrl, String filename){
+    public Response get(URL baseUrl, String filename) {
         try {
             Uri fileUrl = Uri.uri(baseUrl.toString() + filename);
             HttpHandler handler = new ClientHttpHandler();
