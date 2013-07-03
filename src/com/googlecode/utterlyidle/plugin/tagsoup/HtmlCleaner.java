882f772a5a951a8f2fd2ca1e0a9f2af3e0257167
Raymond Barlow <rbarlow@raymanoz.com>
2013-07-03 19:32:44 +0100

    Added HtmlCleaner

---
+++ src/com/googlecode/utterlyidle/plugin/tagsoup/HtmlCleaner.java
@@ -0,0 +1,51 @@
+package com.googlecode.utterlyidle.plugin.tagsoup;
+
+import com.googlecode.totallylazy.LazyException;
+import org.ccil.cowan.tagsoup.Parser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.sax.SAXSource;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+public class HtmlCleaner {
+    public static Node node(String html) {
+        return node(new ByteArrayInputStream(html.getBytes()));
+    }
+
+    public static Node node(InputStream stream) {
+        try {
+            DOMResult result = new DOMResult();
+            transformer().transform(new SAXSource(parser(), new InputSource(stream)), result);
+            return result.getNode();
+        } catch (Exception e) {
+            throw LazyException.lazyException(e);
+        }
+    }
+
+    public static Document document(String html) {
+        return (Document) HtmlCleaner.node(html);
+    }
+
+    public static Document document(InputStream stream) {
+        return (Document) node(stream);
+    }
+
+    private static Transformer transformer() throws TransformerConfigurationException {
+        return TransformerFactory.newInstance().newTransformer();
+    }
+
+    private static XMLReader parser() throws Exception {
+        return new Parser(){{
+                    setFeature(Parser.namespacesFeature, false);
+                    setFeature(Parser.namespacePrefixesFeature, false);
+                }};
+    }
+}
---
+++ test/com/googlecode/utterlyidle/plugin/tagsoup/HtmlCleanerTest.java
@@ -0,0 +1,20 @@
+package com.googlecode.utterlyidle.plugin.tagsoup;
+
+import com.googlecode.totallylazy.Xml;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class HtmlCleanerTest {
+    @Test
+    public void canGetDocumentFromBadlyFormedHtml() throws Exception {
+        String html = "<html><head><body>sheep<br>cheese</body></html>";
+        assertThat(Xml.asString(HtmlCleaner.document(html)), is("<html xmlns:html=\"http://www.w3.org/1999/xhtml\">\n" +
+                "<head>\n" +
+                "<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+                "</head>\n" +
+                "<body>sheep<br clear=\"none\">cheese</body>\n" +
+                "</html>\n"));
+    }
+}
