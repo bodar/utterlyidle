c0fb05754da7675df5b6fdce456e61117322b3b6
Stuart, Raymond <Stuart, Raymond@localhost>
2013-07-17 10:57:15 +0100

    improved error reporting

--- src/com/googlecode/utterlyidle/CompositeEntityWriter.java
+++ src/com/googlecode/utterlyidle/CompositeEntityWriter.java
@@ -7,6 +7,7 @@ import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.Value;
 
 import java.io.OutputStream;
+import java.util.NoSuchElementException;
 
 import static com.googlecode.totallylazy.Unchecked.cast;
 import static com.googlecode.utterlyidle.EntityWriter.functions.asFunction;
@@ -16,7 +17,15 @@ public class CompositeEntityWriter implements EntityWriter<Object>, Value<Sequen
 
     @Override
     public void write(Object entity, OutputStream outputStream) throws Exception {
-        rules.call(entity).call(outputStream);
+        ruleFor(entity).call(outputStream);
+    }
+
+    private Block<OutputStream> ruleFor(final Object entity) throws Exception {
+        try {
+            return rules.call(entity);
+        } catch (NoSuchElementException e) {
+            throw new UnsupportedOperationException("Unknown entity type " + entity.getClass(), e);
+        }
     }
 
     public <T> void add(Predicate<? super T> predicate, final EntityWriter<? super T> writer) {
