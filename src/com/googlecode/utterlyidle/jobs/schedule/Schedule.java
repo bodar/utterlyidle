fa5d62954033e3b535b8d02ce3d93021f3396837
rbarlow <rbarlow@raymanoz.com>
2013-07-23 15:37:28 +0100

    Pulled ScheduleDefinition out into a Definition interface

--- src/com/googlecode/utterlyidle/jobs/schedule/Schedule.java
+++ src/com/googlecode/utterlyidle/jobs/schedule/Schedule.java
@@ -8,21 +8,11 @@ import com.googlecode.totallylazy.time.Clock;
 import java.util.Date;
 import java.util.UUID;
 
-import static com.googlecode.lazyrecords.Keyword.constructors.keyword;
 import static com.googlecode.lazyrecords.Record.constructors.record;
 import static com.googlecode.utterlyidle.jobs.schedule.NextTime.nextTime;
+import static com.googlecode.utterlyidle.jobs.schedule.SchedulesDefinition.scheduleId;
 
 public class Schedule {
-    public static final Keyword<UUID> SCHEDULE_ID = keyword("schedule_id", UUID.class);
-    public static final Keyword<String> REQUEST = keyword("request", String.class);
-    public static final Keyword<String> RESPONSE = keyword("response", String.class);
-    public static final Keyword<String> START = keyword("start", String.class);
-    public static final Keyword<Long> INTERVAL = keyword("interval", Long.class);
-    public static final Keyword<Long> DURATION = keyword("duration", Long.class);
-    public static final Keyword<Date> STARTED = keyword("started", Date.class);
-    public static final Keyword<Date> COMPLETED = keyword("completed", Date.class);
-    public static final Keyword<Boolean> RUNNING = keyword("running", Boolean.class);
-
     private final Record record;
 
     private Schedule(Record record) {
@@ -30,31 +20,31 @@ public class Schedule {
     }
 
     public static Schedule schedule(UUID id) {
-        return new Schedule(record().set(SCHEDULE_ID, id));
+        return new Schedule(record().set(scheduleId, id));
     }
 
     public Schedule interval(Long interval) {
-        return set(INTERVAL, interval);
+        return set(SchedulesDefinition.interval, interval);
     }
 
     public Schedule request(String request) {
-        return set(REQUEST, request);
+        return set(SchedulesDefinition.request, request);
     }
 
     public Schedule response(String response) {
-        return set(RESPONSE, response);
+        return set(SchedulesDefinition.response, response);
     }
 
     public Schedule started(Date started) {
-        return set(STARTED, started);
+        return set(SchedulesDefinition.started, started);
     }
 
     public Schedule completed(Date completed) {
-        return set(COMPLETED, completed);
+        return set(SchedulesDefinition.completed, completed);
     }
 
     public Schedule start(String start) {
-        return set(START, start);
+        return set(SchedulesDefinition.start, start);
     }
 
     public static Date start(String time, Clock clock) {
@@ -62,11 +52,11 @@ public class Schedule {
     }
 
     public Schedule running(boolean running) {
-        return set(RUNNING, running);
+        return set(SchedulesDefinition.running, running);
     }
 
     public Schedule duration(Long duration) {
-        return set(DURATION, duration);
+        return set(SchedulesDefinition.duration, duration);
     }
 
     public Record toRecord() {
@@ -80,7 +70,7 @@ public class Schedule {
     public static Callable1<Record, UUID> asScheduleId() {
         return new Callable1<Record, UUID>() {
             public UUID call(Record record) throws Exception {
-                return record.get(SCHEDULE_ID);
+                return record.get(scheduleId);
             }
         };
     }
