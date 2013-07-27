a7693bb227743d1989acb40ad9fc9649e1f4d20b
Stu <Stu@localhost>
2013-07-27 19:09:07 +0100

    Be more forgiving to poor plebs on Windows

--- test/com/googlecode/utterlyidle/tagsoup/HtmlCleanerTest.java
+++ test/com/googlecode/utterlyidle/tagsoup/HtmlCleanerTest.java
@@ -10,11 +10,6 @@ public class HtmlCleanerTest {
     @Test
     public void canGetDocumentFromBadlyFormedHtml() throws Exception {
         String html = "<html><head><body>sheep<br>cheese</body></html>";
-        assertThat(Xml.asString(HtmlCleaner.document(html)), is("<html xmlns:html=\"http://www.w3.org/1999/xhtml\">\n" +
-                "<head>\n" +
-                "<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                "</head>\n" +
-                "<body>sheep<br clear=\"none\">cheese</body>\n" +
-                "</html>\n"));
+        assertThat(Xml.asString(HtmlCleaner.document(html)), is(String.format("<html xmlns:html=\"http://www.w3.org/1999/xhtml\">%n<head>%n<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">%n</head>%n<body>sheep<br clear=\"none\">cheese</body>%n</html>%n")));
     }
 }
