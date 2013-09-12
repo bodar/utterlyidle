f27d9a46866f36ab007c34eacaea503907112288
Daniel Worthington-Bodart <dan@bodar.com>
2013-09-12 12:33:57 +0100

    Added support for first request not being absolute

--- src/com/googlecode/utterlyidle/html/RelativeUrlHandler.java
+++ src/com/googlecode/utterlyidle/html/RelativeUrlHandler.java
@@ -9,11 +9,10 @@ import com.googlecode.utterlyidle.Response;
 
 import static com.googlecode.totallylazy.Strings.isEmpty;
 import static com.googlecode.utterlyidle.RequestBuilder.modify;
-import static com.googlecode.utterlyidle.annotations.HttpMethod.GET;
 
 public class RelativeUrlHandler implements HttpHandler {
     private final HttpHandler httpHandler;
-    private Uri currentUri;
+    private Uri currentUri = Uri.uri("/");
 
     public RelativeUrlHandler(HttpHandler httpHandler) {
         this.httpHandler = httpHandler;
@@ -21,23 +20,21 @@ public class RelativeUrlHandler implements HttpHandler {
 
     public Response handle(Request request) throws Exception {
         RequestBuilder builder = modify(request);
-        if (currentUri != null) {
-            if(isEmpty(builder.uri().scheme())) {
-                builder.uri(builder.uri().scheme(currentUri.scheme()));
+        if (isEmpty(builder.uri().scheme())) {
+            builder.uri(builder.uri().scheme(currentUri.scheme()));
+        }
+        if (isEmpty(builder.uri().authority())) {
+            builder.uri(builder.uri().authority(currentUri.authority()));
+        }
+        if (isEmpty(builder.uri().path())) {
+            if (Strings.isEmpty(builder.uri().query())) {
+                builder.uri(currentUri);
+            } else {
+                builder.uri(currentUri.query(builder.uri().query()));
             }
-            if(isEmpty(builder.uri().authority())) {
-                builder.uri(builder.uri().authority(currentUri.authority()));
-            }
-            if (isEmpty(builder.uri().path())) {
-                if (Strings.isEmpty(builder.uri().query())) {
-                    builder.uri(currentUri);
-                } else {
-                    builder.uri(currentUri.query(builder.uri().query()));
-                }
-            } else if (builder.uri().isRelative()) {
-                String absolutePath = currentUri.mergePath(builder.uri().path()).path();
-                builder.uri(builder.uri().mergePath(absolutePath));
-            }
+        } else if (builder.uri().isRelative()) {
+            String absolutePath = currentUri.mergePath(builder.uri().path()).path();
+            builder.uri(builder.uri().mergePath(absolutePath));
         }
         currentUri = builder.uri();
         return httpHandler.handle(builder.build());
