3e180d69598649792576adeebba5453a989b2a2f
westtob <westtob@UKLONWS939613.uk.db.com>
2013-09-12 08:38:06 +0100

    adding excel media type (application/vnd.ms-excel)

--- src/com/googlecode/utterlyidle/MediaType.java
+++ src/com/googlecode/utterlyidle/MediaType.java
@@ -9,8 +9,9 @@ public class MediaType {
     public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_PDF = "application/pdf";
     public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
-    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
     public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
+    public static final String APPLICATION_MS_EXCEL = "application/vnd.ms-excel";
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
     public static final String TEXT_PLAIN = "text/plain";
     public static final String TEXT_CSV = "text/csv";
     public static final String TEXT_XML = "text/xml";
