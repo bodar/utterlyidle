80a022f40f11924172d4dbf823b54e155c7b4fbb
westtob <westtob@UKLONWS939613.uk.db.com>
2013-09-12 08:51:21 +0100

    fixes #11, match @Consumes based on value *starting with* the request's value. Means it will ignore "; charset=UTF-8" for example.

---
+++ test/com/googlecode/utterlyidle/ConsumesMimeMatcherTest.java
@@ -0,0 +1,47 @@
+package com.googlecode.utterlyidle;
+
+import com.googlecode.totallylazy.Predicate;
+import com.googlecode.totallylazy.Uri;
+import org.junit.Test;
+
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.totallylazy.matchers.Matchers.is;
+import static com.googlecode.utterlyidle.HeaderParameters.headerParameters;
+import static com.googlecode.utterlyidle.HttpHeaders.*;
+import static com.googlecode.utterlyidle.MediaType.APPLICATION_ATOM_XML;
+import static com.googlecode.utterlyidle.MediaType.APPLICATION_FORM_URLENCODED;
+import static com.googlecode.utterlyidle.MediaType.APPLICATION_JSON;
+import static com.googlecode.utterlyidle.Requests.request;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ConsumesMimeMatcherTest {
+
+    private final HeaderParameters headers = headerParameters().add(CONTENT_TYPE, APPLICATION_FORM_URLENCODED);
+    private final Uri uri = Uri.uri("http://example.com");
+    private final Request request = request("GET", uri, headers, null);
+
+    @Test
+    public void simpleMatches() throws Exception {
+        Predicate<Request> matcher = new ConsumesMimeMatcher(sequence(APPLICATION_FORM_URLENCODED));
+        assertThat(matcher.matches(request), is(true));
+    }
+
+    @Test
+    public void matchesWithManyCandidates() throws Exception {
+        Predicate<Request> matcher = new ConsumesMimeMatcher(sequence(APPLICATION_ATOM_XML, APPLICATION_FORM_URLENCODED, APPLICATION_JSON));
+        assertThat(matcher.matches(request), is(true));
+    }
+
+    @Test
+    public void matchesIgnoringCharset() throws Exception {
+        Predicate<Request> matcher = new ConsumesMimeMatcher(sequence(APPLICATION_ATOM_XML, APPLICATION_FORM_URLENCODED + "; charset=UTF-8", APPLICATION_JSON));
+        assertThat(matcher.matches(request), is(true));
+    }
+
+    @Test
+    public void simpleMisMatch() throws Exception {
+        Predicate<Request> matcher = new ConsumesMimeMatcher(sequence(APPLICATION_ATOM_XML));
+        assertThat(matcher.matches(request), is(false));
+    }
+
+}
