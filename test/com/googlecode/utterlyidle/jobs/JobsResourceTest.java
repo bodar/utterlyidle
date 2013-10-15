b9cd750599d1355e8023f5165338017e9b5a6db8
Daniel Worthington-Bodart <dan@bodar.com>
2013-10-15 15:18:16 +0100

    Added result link to Jobs response

--- test/com/googlecode/utterlyidle/jobs/JobsResourceTest.java
+++ test/com/googlecode/utterlyidle/jobs/JobsResourceTest.java
@@ -2,31 +2,52 @@ package com.googlecode.utterlyidle.jobs;
 
 import com.googlecode.funclate.Model;
 import com.googlecode.totallylazy.Uri;
+import com.googlecode.totallylazy.time.Clock;
+import com.googlecode.totallylazy.time.Dates;
+import com.googlecode.totallylazy.time.StoppedClock;
 import com.googlecode.utterlyidle.Application;
 import com.googlecode.utterlyidle.HttpHeaders;
 import com.googlecode.utterlyidle.Request;
+import com.googlecode.utterlyidle.RequestBuilder;
 import com.googlecode.utterlyidle.Response;
+import com.googlecode.utterlyidle.ResponseBuilder;
+import com.googlecode.utterlyidle.Status;
 import com.googlecode.utterlyidle.examples.HelloWorldApplication;
 import org.junit.Test;
 
+import java.text.ParseException;
+
 import static com.googlecode.funclate.Model.persistent.parse;
-import static com.googlecode.totallylazy.Strings.string;
 import static com.googlecode.totallylazy.matchers.Matchers.is;
 import static com.googlecode.totallylazy.proxy.Call.method;
 import static com.googlecode.totallylazy.proxy.Call.on;
+import static com.googlecode.totallylazy.time.Dates.date;
 import static com.googlecode.utterlyidle.RequestBuilder.get;
 import static com.googlecode.utterlyidle.RequestBuilder.modify;
 import static com.googlecode.utterlyidle.RequestBuilder.post;
-import static com.googlecode.utterlyidle.Status.ACCEPTED;
+import static com.googlecode.utterlyidle.Status.CREATED;
 import static com.googlecode.utterlyidle.Status.OK;
 import static com.googlecode.utterlyidle.Status.SEE_OTHER;
 import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.relativeUriOf;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class JobsResourceTest {
     Application application = new HelloWorldApplication();
+
+    @Test
+    public void statusCodesMapCorrectly() throws Exception {
+        Clock clock = new StoppedClock(date(2001, 1, 2));
+
+        CreatedJob createdJob = CreatedJob.createJob(RequestBuilder.get("").build(), clock);
+        assertThat(JobsResource.status(createdJob), is(Status.CREATED));
+
+        RunningJob runningJob = createdJob.start(clock);
+        assertThat(JobsResource.status(runningJob), is(Status.ACCEPTED));
+
+        CompletedJob completedJob = runningJob.complete(ResponseBuilder.response().build(), clock);
+        assertThat(JobsResource.status(completedJob), is(Status.OK));
+    }
 
     @Test
     public void canCreateAJobAndInspectIt() throws Exception {
@@ -37,7 +58,7 @@ public class JobsResourceTest {
         Response response = create(post("some/url").build());
         assertThat(response.status(), is(SEE_OTHER));
         String location = response.headers().getValue(HttpHeaders.LOCATION);
-        assertThat(application.handle(get(location).build()).status(), is(ACCEPTED));
+        assertThat(application.handle(get(location).build()).status(), is(CREATED));
 
         completer.job.call();
 
@@ -45,14 +66,16 @@ public class JobsResourceTest {
 
         Response job = application.handle(get(location).build());
 
-        String json = job.entity().toString();
-        System.out.println("json = " + json);
-        Object completed = parse(json).get("completed", Object.class);
-        System.out.println("completed = " + completed);
-        assertThat(completed, instanceOf(String.class));
-
         assertThat(job.status(), is(OK));
 
+        try {
+            Model json = parse(job.entity().toString());
+            Dates.RFC3339withMilliseconds().parse(json.get("created", String.class));
+            Dates.RFC3339withMilliseconds().parse(json.get("started", String.class));
+            Dates.RFC3339withMilliseconds().parse(json.get("completed", String.class));
+        } catch (ParseException e) {
+            fail("Dates should be JSON ISO 8601 / RFC 3339 format");
+        }
     }
 
     @Test
