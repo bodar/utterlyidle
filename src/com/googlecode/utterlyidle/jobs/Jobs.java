db3a06dbd36fef99db5e4fe9946ea9e87ed6ec5f
Daniel Worthington-Bodart <dan@bodar.com>
2013-11-12 16:15:19 +0000

    Added support to plugin in custom Jobs Storage

--- src/com/googlecode/utterlyidle/jobs/Jobs.java
+++ src/com/googlecode/utterlyidle/jobs/Jobs.java
@@ -1,13 +1,9 @@
 package com.googlecode.utterlyidle.jobs;
 
-import com.googlecode.totallylazy.Sequence;
 import com.googlecode.utterlyidle.Request;
 
 public interface Jobs {
-    Sequence<Job> jobs();
-
     void deleteAll();
 
     Job create(Request request);
-
 }
