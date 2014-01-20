35f09391c818e6a6e56452e0e008b5b7e9dcbc6f
Dan & Stu <Dan & Stu@localhost>
2014-01-20 15:42:09 +0000

    Logging parameter match failures in debug.

--- src/com/googlecode/utterlyidle/ParametersExtractor.java
+++ src/com/googlecode/utterlyidle/ParametersExtractor.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.googlecode.totallylazy.Debug.debugging;
 import static com.googlecode.yadic.resolvers.Resolvers.create;
 
 public class ParametersExtractor implements RequestExtractor<Object[]> {
@@ -32,6 +33,7 @@ public class ParametersExtractor implements RequestExtractor<Object[]> {
             extract(request);
             return true;
         } catch (Exception e) {
+            if (debugging()) e.printStackTrace();
             return false;
         }
     }
