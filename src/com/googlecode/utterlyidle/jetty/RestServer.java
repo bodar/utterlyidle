5d24e5f0f8be98257bc430f7100923adfe5b95d7
tom <tom@localhost>
2014-02-12 11:27:51 +0000

    moving jetty from socket connector to select channel selector

--- src/com/googlecode/utterlyidle/jetty/RestServer.java
+++ src/com/googlecode/utterlyidle/jetty/RestServer.java
@@ -13,6 +13,7 @@ import com.googlecode.utterlyidle.servlet.ApplicationServlet;
 import com.googlecode.utterlyidle.servlet.ServletModule;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.webapp.WebAppContext;
 import org.mortbay.thread.QueuedThreadPool;
@@ -117,10 +118,10 @@ public class RestServer implements com.googlecode.utterlyidle.Server {
 
     private Server createServer(ServerConfiguration serverConfig) {
         Server server = new Server();
-        SocketConnector socketConnector = new SocketConnector();
-        socketConnector.setPort(serverConfig.port());
-        socketConnector.setHost(serverConfig.bindAddress().getHostAddress());
-        server.addConnector(socketConnector);
+        SelectChannelConnector selectChannelConnector = new SelectChannelConnector();
+        selectChannelConnector.setPort(serverConfig.port());
+        selectChannelConnector.setHost(serverConfig.bindAddress().getHostAddress());
+        server.addConnector(selectChannelConnector);
         server.setThreadPool(new QueuedThreadPool(serverConfig.maxThreadNumber()));
         return server;
     }
