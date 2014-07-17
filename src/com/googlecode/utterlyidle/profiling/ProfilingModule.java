2af6a1d4c01417f78df92133305d2e718d3cc0e7
Daniel Worthington-Bodart <dan@bodar.com>
2014-07-17 15:57:09 +0100

    Fixed profiling module to not care what scope LR logger is

--- src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
@@ -3,10 +3,11 @@ package com.googlecode.utterlyidle.profiling;
 import com.googlecode.lazyrecords.Logger;
 import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.handlers.HttpClient;
+import com.googlecode.utterlyidle.modules.ApplicationScopedModule;
 import com.googlecode.utterlyidle.modules.RequestScopedModule;
 import com.googlecode.yadic.Container;
 
-public class ProfilingModule implements RequestScopedModule {
+public class ProfilingModule implements RequestScopedModule, ApplicationScopedModule {
     @Override
     public Container addPerRequestObjects(Container container) throws Exception {
         container.addActivator(ProfilingData.class, ProfilingDataActivator.class);
@@ -15,6 +16,14 @@ public class ProfilingModule implements RequestScopedModule {
         }
         container.decorate(HttpHandler.class, ProfilingHandler.class);
         container.decorate(HttpClient.class, ProfilingClient.class);
+        return container;
+    }
+
+    @Override
+    public Container addPerApplicationObjects(final Container container) throws Exception {
+        if (container.contains(Logger.class)) {
+            container.decorate(Logger.class, ProfilingLogger.class);
+        }
         return container;
     }
 }
