dc7826c42fe35e040460bb0151bcc5a25d6ed2e7
Daniel Worthington-Bodart <dan@bodar.com>
2014-07-18 09:50:17 +0100

    Removed profiled logger using application scope as it will never work

--- src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
+++ src/com/googlecode/utterlyidle/profiling/ProfilingModule.java
@@ -3,11 +3,10 @@ package com.googlecode.utterlyidle.profiling;
 import com.googlecode.lazyrecords.Logger;
 import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.handlers.HttpClient;
-import com.googlecode.utterlyidle.modules.ApplicationScopedModule;
 import com.googlecode.utterlyidle.modules.RequestScopedModule;
 import com.googlecode.yadic.Container;
 
-public class ProfilingModule implements RequestScopedModule, ApplicationScopedModule {
+public class ProfilingModule implements RequestScopedModule {
     @Override
     public Container addPerRequestObjects(Container container) throws Exception {
         container.addActivator(ProfilingData.class, ProfilingDataActivator.class);
@@ -16,14 +15,6 @@ public class ProfilingModule implements RequestScopedModule, ApplicationScopedMo
         }
         container.decorate(HttpHandler.class, ProfilingHandler.class);
         container.decorate(HttpClient.class, ProfilingClient.class);
-        return container;
-    }
-
-    @Override
-    public Container addPerApplicationObjects(final Container container) throws Exception {
-        if (container.contains(Logger.class)) {
-            container.decorate(Logger.class, ProfilingLogger.class);
-        }
         return container;
     }
 }
