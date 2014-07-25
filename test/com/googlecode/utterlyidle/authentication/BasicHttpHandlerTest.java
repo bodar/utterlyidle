4867c6a164f830073df34ec486a5acff63fa24a6
Daniel Worthington-Bodart <dan@bodar.com>
2014-07-25 14:15:32 +0100

    Clean up

--- test/com/googlecode/utterlyidle/authentication/BasicHttpHandlerTest.java
+++ test/com/googlecode/utterlyidle/authentication/BasicHttpHandlerTest.java
@@ -6,7 +6,6 @@ import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.HttpHeaders;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.Response;
-import com.googlecode.utterlyidle.Status;
 import org.junit.Test;
 
 import static com.googlecode.totallylazy.Strings.bytes;
@@ -18,43 +17,38 @@ import static com.googlecode.utterlyidle.Status.OK;
 import static com.googlecode.utterlyidle.Status.UNAUTHORIZED;
 import static com.googlecode.utterlyidle.authentication.Credential.credential;
 import static com.googlecode.utterlyidle.handlers.ReturnResponseHandler.returns;
-import static com.googlecode.utterlyidle.handlers.ReturnResponseHandler.returnsResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BasicHttpHandlerTest {
     @Test
     public void returnsUnauthorizedWhenNoCredentials() throws Exception {
-        BaseUri baseUri = BaseUri.baseUri("http://example.com/");
-        Credentials credentials = Credentials.credentials(credential("dan", "blah"));
-        HttpHandler handler = new BasicHttpHandler(throwingHandler(), baseUri, credentials);
-
-        Response response = handler.handle(get("/").build());
+        Response response = basicServer(throwingHandler()).handle(get("/").build());
         assertThat(response.status(), is(UNAUTHORIZED));
         assertThat(response.headers().getValue(WWW_AUTHENTICATE), is("Basic realm=example.com"));
     }
 
     @Test
     public void returnsUnauthorizedWhenCredentialsDontMatch() throws Exception {
-        BaseUri baseUri = BaseUri.baseUri("http://example.com/");
-        Credentials credentials = Credentials.credentials(credential("dan", "right"));
-        HttpHandler handler = new BasicHttpHandler(throwingHandler(), baseUri, credentials);
-
-        Response response = handler.handle(get("/").header(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encode(bytes("dan:wrong"))).build());
+        Response response = basicServer(throwingHandler()).
+                handle(get("/").header(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encode(bytes("dan:wrong"))).build());
         assertThat(response.status(), is(UNAUTHORIZED));
         assertThat(response.headers().getValue(WWW_AUTHENTICATE), is("Basic realm=example.com"));
     }
 
     @Test
     public void letsRequestThroughWhenCredentialsMatch() throws Exception {
-        BaseUri baseUri = BaseUri.baseUri("http://example.com/");
-        Credentials credentials = Credentials.credentials(credential("dan", "right"));
-        HttpHandler handler = new BasicHttpHandler(returns(response(OK)), baseUri, credentials);
-
-        Response response = handler.handle(get("/").header(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encode(bytes("dan:right"))).build());
+        Response response = basicServer(returns(response(OK))).
+                handle(get("/").header(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encode(bytes("dan:right"))).build());
         assertThat(response.status(), is(OK));
     }
 
-    private HttpHandler throwingHandler() {
+    public static HttpHandler basicServer(final HttpHandler handler) {
+        BaseUri baseUri = BaseUri.baseUri("http://example.com/");
+        Credentials credentials = Credentials.credentials(credential("dan", "right"));
+        return new BasicHttpHandler(handler, baseUri, credentials);
+    }
+
+    public static HttpHandler throwingHandler() {
         return new HttpHandler() {
             @Override
             public Response handle(final Request request) throws Exception {
