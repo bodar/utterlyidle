6be21192376621867bb7fb91666dfba50a94ec71
Daniel Worthington-Bodart <dan@bodar.com>
2014-07-25 17:11:08 +0100

    Fixed Basic Client authentication

--- src/com/googlecode/utterlyidle/authentication/BasicHttpHandler.java
+++ src/com/googlecode/utterlyidle/authentication/BasicHttpHandler.java
@@ -1,25 +1,17 @@
 package com.googlecode.utterlyidle.authentication;
 
-import com.googlecode.totallylazy.Function1;
-import com.googlecode.totallylazy.Option;
-import com.googlecode.totallylazy.Strings;
-import com.googlecode.totallylazy.parser.Parser;
-import com.googlecode.totallylazy.parser.Parsers;
-import com.googlecode.utterlyidle.Base64;
 import com.googlecode.utterlyidle.BaseUri;
 import com.googlecode.utterlyidle.HttpHandler;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.Response;
 
-import static com.googlecode.totallylazy.Characters.alphaNumeric;
 import static com.googlecode.totallylazy.Predicates.in;
 import static com.googlecode.totallylazy.parser.Parsers.isChar;
-import static com.googlecode.totallylazy.parser.Parsers.ws;
 import static com.googlecode.utterlyidle.HttpHeaders.AUTHORIZATION;
 import static com.googlecode.utterlyidle.HttpHeaders.WWW_AUTHENTICATE;
 import static com.googlecode.utterlyidle.ResponseBuilder.response;
 import static com.googlecode.utterlyidle.Status.UNAUTHORIZED;
-import static com.googlecode.utterlyidle.authentication.BasicHttpHandler.Grammar.parseCredential;
+import static com.googlecode.utterlyidle.authentication.BasicGrammar.parseCredential;
 import static java.lang.String.format;
 
 public class BasicHttpHandler implements HttpHandler {
@@ -48,26 +40,4 @@ public class BasicHttpHandler implements HttpHandler {
                 flatMap(parseCredential).exists(in(credentials.value()));
     }
 
-    interface Grammar {
-        Parser<Void> Scheme = Parsers.string("Basic").ignore();
-
-        Parser<Credential> BasicCookie = isChar(alphaNumeric.or(in('/', '+', '='))).many1().
-                map(Parsers.toString).
-                map(new Function1<String, Credential>() {
-                    @Override
-                    public Credential call(final String raw) throws Exception {
-                        String[] pair = Strings.string(Base64.decode(raw)).split(":");
-                        return Credential.credential(pair[0], pair[1]);
-                    }
-                });
-
-        Parser<Credential> Credentials = Scheme.next(ws(BasicCookie));
-
-        Function1<String, Option<Credential>> parseCredential = new Function1<String, Option<Credential>>() {
-            @Override
-            public Option<Credential> call(final String auth) throws Exception {
-                return Credentials.parse(auth).option();
-            }
-        };
-    }
 }
