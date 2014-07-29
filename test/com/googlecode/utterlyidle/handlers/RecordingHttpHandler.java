08046f70b666dded2c1e34af36929815c5844484
Daniel Worthington-Bodart <dan@bodar.com>
2014-07-29 13:31:02 +0100

    Simplified Basic Handler

--- test/com/googlecode/utterlyidle/handlers/RecordingHttpHandler.java
+++ test/com/googlecode/utterlyidle/handlers/RecordingHttpHandler.java
@@ -8,6 +8,9 @@ import com.googlecode.utterlyidle.Response;
 import static com.googlecode.totallylazy.collections.PersistentList.constructors.cons;
 import static com.googlecode.totallylazy.collections.PersistentList.constructors.empty;
 import static com.googlecode.totallylazy.collections.PersistentList.constructors.reverse;
+import static com.googlecode.utterlyidle.ResponseBuilder.response;
+import static com.googlecode.utterlyidle.Status.OK;
+import static com.googlecode.utterlyidle.handlers.ReturnResponseHandler.returns;
 
 public class RecordingHttpHandler implements HttpHandler, HttpClient {
     private PersistentList<Request> requests = empty();
@@ -19,6 +22,10 @@ public class RecordingHttpHandler implements HttpHandler, HttpClient {
 
     public static RecordingHttpHandler recordingHttpHandler(HttpHandler delegate) {
         return new RecordingHttpHandler(delegate);
+    }
+
+    public static RecordingHttpHandler recordingHttpHandler() {
+        return recordingHttpHandler(returns(response(OK).build()));
     }
 
     public Response handle(Request request) throws Exception {
