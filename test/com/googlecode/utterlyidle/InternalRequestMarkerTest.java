ef175b80ef9af048916105eb023e5c6e170bd279
Daniel Worthington-Bodart <dan@bodar.com>
2015-10-30 14:04:21 +0000

    Migrating from RequestBuilder to Request.Builder

--- test/com/googlecode/utterlyidle/InternalRequestMarkerTest.java
+++ test/com/googlecode/utterlyidle/InternalRequestMarkerTest.java
@@ -1,7 +1,9 @@
 package com.googlecode.utterlyidle;
 
+import com.googlecode.utterlyidle.Request.Builder;
 import org.junit.Test;
 
+import static com.googlecode.utterlyidle.Request.Builder.get;
 import static com.googlecode.utterlyidle.handlers.ApplicationId.applicationId;
 import static java.util.UUID.randomUUID;
 import static org.hamcrest.Matchers.is;
@@ -11,25 +13,25 @@ public class InternalRequestMarkerTest {
     @Test
     public void supportsMarkingARequestAsInternal() throws Exception {
         InternalRequestMarker marker = new InternalRequestMarker(applicationId());
-        final Request request = marker.markAsInternal(RequestBuilder.get("/testInternalUrl").build());
+        final Request request = marker.markAsInternal(get("/testInternalUrl"));
         assertThat(marker.isInternal(request), is(true));
     }
 
     @Test
     public void indicatesRequestIsNotInternalForRequestWithNoForwardedForHeader() throws Exception {
         InternalRequestMarker marker = new InternalRequestMarker(applicationId());
-        assertThat(marker.isInternal(RequestBuilder.get("/testInternalUrl").build()), is(false));
+        assertThat(marker.isInternal(get("/testInternalUrl")), is(false));
     }
 
     @Test
     public void indicatedRequestIsNotInternalForForwardedForValueNotEqualToApplicationId() throws Exception {
         InternalRequestMarker marker = new InternalRequestMarker(applicationId());
-        assertThat(marker.isInternal(RequestBuilder.get("/testInternalUrl").header("X-Forwarded-For", randomUUID()).build()), is(false));
+        assertThat(marker.isInternal(get("/testInternalUrl", Builder.header("X-Forwarded-For", randomUUID()))), is(false));
     }
 
     @Test
     public void shouldWorkWhenXForwardedForAlreadyInRequest() throws Exception {
-        Request request = RequestBuilder.get("/").header(HttpHeaders.X_FORWARDED_FOR, "quidgeybo").build();
+        Request request = Request.Builder.get("/", Request.Builder.header(HttpHeaders.X_FORWARDED_FOR, "quidgeybo"));
         InternalRequestMarker marker = new InternalRequestMarker(applicationId());
         Request internalRequest = marker.markAsInternal(request);
         assertThat(marker.isInternal(internalRequest), is(true));
