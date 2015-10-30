6d488cd8e96e28045e0646cb43a6f613fad9f9e3
Daniel Worthington-Bodart <dan@bodar.com>
2015-10-30 13:03:01 +0000

    Migrating from RequestBuilder to Request.Builder

--- test/com/googlecode/utterlyidle/AuditTest.java
+++ test/com/googlecode/utterlyidle/AuditTest.java
@@ -2,15 +2,13 @@ package com.googlecode.utterlyidle;
 
 import com.googlecode.totallylazy.Pair;
 import com.googlecode.utterlyidle.handlers.Auditor;
-import com.googlecode.utterlyidle.handlers.Auditors;
 import com.googlecode.utterlyidle.modules.AuditModule;
 import org.junit.Test;
 
-import java.net.URI;
 import java.util.Date;
 
 import static com.googlecode.utterlyidle.ApplicationBuilder.application;
-import static com.googlecode.utterlyidle.RequestBuilder.get;
+import static com.googlecode.utterlyidle.Request.Builder.get;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -19,7 +17,7 @@ public class AuditTest {
     public void recordsRequestAndResponse() throws Exception {
         Application application = application().add(auditModule()).build();
 
-        Request request = get("").build();
+        Request request = get("");
         Response response = application.handle(request);
 
         assertThat(TestAuditor.receivedRequest, is(request));
--- test/com/googlecode/utterlyidle/handlers/CachingTest.java
+++ test/com/googlecode/utterlyidle/handlers/CachingTest.java
@@ -16,8 +16,8 @@ import static com.googlecode.utterlyidle.HttpHeaders.EXPIRES;
 import static com.googlecode.utterlyidle.MediaType.TEXT_CSS;
 import static com.googlecode.utterlyidle.MediaType.TEXT_JAVASCRIPT;
 import static com.googlecode.utterlyidle.PathMatcher.path;
-import static com.googlecode.utterlyidle.RequestBuilder.get;
-import static com.googlecode.utterlyidle.RequestBuilder.post;
+import static com.googlecode.utterlyidle.Request.Builder.get;
+import static com.googlecode.utterlyidle.Request.Builder.post;
 import static com.googlecode.utterlyidle.Response.methods.header;
 import static com.googlecode.utterlyidle.ResponseBuilder.response;
 import static com.googlecode.utterlyidle.handlers.CachePolicy.cachePolicy;
@@ -30,7 +30,7 @@ public class CachingTest {
     @Test
     public void setsCacheControlHeaders() throws Exception {
         HttpHandler handler = new CacheControlHandler(returnsResponse(response().header(DATE, Dates.RFC822().format(date(2000, 1, 1)))), cachePolicy(60).add(always()));
-        Response response = handler.handle(get("/").build());
+        Response response = handler.handle(get("/"));
         assertThat(header(response, CACHE_CONTROL), is("public, max-age=60"));
         assertThat(header(response, EXPIRES), is("Sat, 01 Jan 2000 00:01:00 GMT"));
     }
@@ -38,7 +38,7 @@ public class CachingTest {
     @Test
     public void disablesCachingWhenItDoesNotMatch() throws Exception {
         HttpHandler handler = new CacheControlHandler(returnsResponse(response()), cachePolicy(60));
-        Response response = handler.handle(get("/").build());
+        Response response = handler.handle(get("/"));
         assertThat(header(response, CACHE_CONTROL), is("private, must-revalidate"));
         assertThat(header(response, EXPIRES), is("0"));
     }
@@ -46,34 +46,34 @@ public class CachingTest {
     @Test
     public void passesExistingCacheHeaderThrough() throws Exception {
         HttpHandler handler = new CacheControlHandler(returnsResponse(response().header(CACHE_CONTROL, "foo")), null);
-        Response response = handler.handle(get("/").build());
+        Response response = handler.handle(get("/"));
         assertThat(header(response, CACHE_CONTROL), is("foo"));
         assertThat(response.headers().contains(EXPIRES), is(false));
 
         HttpHandler handler1 = new CacheControlHandler(returnsResponse(response().header(EXPIRES, "bar")), null);
-        Response response1 = handler1.handle(get("/").build());
+        Response response1 = handler1.handle(get("/"));
         assertThat(header(response1, EXPIRES), is("bar"));
         assertThat(response1.headers().contains(CACHE_CONTROL), is(false));
     }
 
     @Test
     public void canControlPolicyBasedOnPath() throws Exception {
-        assertThat(cachePolicy(60).add(path("foo")).matches(Pair.pair(get("/foo").build(), Responses.response())), is(true));
-        assertThat(cachePolicy(60).add(path("bar")).matches(Pair.pair(get("/foo").build(), Responses.response())), is(false));
+        assertThat(cachePolicy(60).add(path("foo")).matches(Pair.pair(get("/foo"), Responses.response())), is(true));
+        assertThat(cachePolicy(60).add(path("bar")).matches(Pair.pair(get("/foo"), Responses.response())), is(false));
     }
 
     @Test
     public void canControlPolicyBasedOnContentType() throws Exception {
         assertThat(cachePolicy(60).add(contentType(TEXT_CSS).or(contentType(TEXT_JAVASCRIPT))).
-                matches(Pair.pair(get("/foo").build(), response().contentType(TEXT_JAVASCRIPT).build())), is(true));
+                matches(Pair.pair(get("/foo"), response().contentType(TEXT_JAVASCRIPT).build())), is(true));
         assertThat(cachePolicy(60).add(contentType(TEXT_CSS).or(contentType(TEXT_JAVASCRIPT))).
-                matches(Pair.pair(get("/foo").build(), response().contentType(MediaType.APPLICATION_ATOM_XML).build())), is(false));
+                matches(Pair.pair(get("/foo"), response().contentType(MediaType.APPLICATION_ATOM_XML).build())), is(false));
     }
 
     @Test
     public void onlyAppliesForGetRequestsAndResponseIsOk() throws Exception {
         HttpHandler handler = new CacheControlHandler(returnsResponse(response() .header(DATE, Dates.RFC822().format(date(2000, 1, 1)))), cachePolicy(60));
-        Response response = handler.handle(post("/").build());
+        Response response = handler.handle(post("/"));
         assertThat(response.headers().contains(CACHE_CONTROL), is(false));
         assertThat(response.headers().contains(EXPIRES), is(false));
     }
