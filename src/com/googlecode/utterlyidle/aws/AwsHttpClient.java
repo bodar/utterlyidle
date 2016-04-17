2bf6da043287a399d9c51c66c1d7fae4155eb28d
Daniel Worthington-Bodart <dan@bodar.com>
2016-04-17 08:56:25 +0100

    Minor clean up

--- src/com/googlecode/utterlyidle/aws/AwsHttpClient.java
+++ src/com/googlecode/utterlyidle/aws/AwsHttpClient.java
@@ -32,17 +32,15 @@ public class AwsHttpClient implements HttpClient {
     public Response handle(Request request) throws Exception {
         AwsRequestDate date = AwsRequestDate.awsRequestDate(clock.now());
 
-        Request fullRequest = modify(request,
-                HttpMessage.Builder.header("host", request.uri().host()),
-                HttpMessage.Builder.header("x-amz-date", date.full())
-        );
+        Request fullRequest = request.
+                header("host", request.uri().host()).
+                header("x-amz-date", date.full());
 
         AwsCanonicalRequest canonicalRequest = new AwsCanonicalRequest(fullRequest);
 
-        Request signedRequest = modify(fullRequest,
-                HttpMessage.Builder.header("Authorization", buildAuthHeader(canonicalRequest, date)),
-                HttpMessage.Builder.header("x-amz-content-sha256", canonicalRequest.payloadHash())
-        );
+        Request signedRequest = fullRequest.
+                header("Authorization", buildAuthHeader(canonicalRequest, date)).
+                header("x-amz-content-sha256", canonicalRequest.payloadHash());
 
         return handler.handle(signedRequest);
     }
