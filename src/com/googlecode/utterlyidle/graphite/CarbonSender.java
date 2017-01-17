251f05572be507b5eca205061296c447be4fb4cb
Daniel Worthington-Bodart <dan@bodar.com>
2017-01-17 14:34:53 +0000

    Renamed guage

--- src/com/googlecode/utterlyidle/graphite/CarbonSender.java
+++ src/com/googlecode/utterlyidle/graphite/CarbonSender.java
@@ -12,7 +12,11 @@ import java.nio.channels.SocketChannel;
 import static java.lang.String.format;
 
 public interface CarbonSender extends Closeable {
-    void counter(String name, long value) throws IOException;
+    /**
+     * A gauge is an instantaneous measurement of a value, like the gas gauge in a car. It differs from a counter by being
+     * calculated at the client rather than the server.
+     **/
+    void gauge(String name, long value) throws IOException;
 
     @Override
     default void close() throws IOException {
@@ -22,7 +26,7 @@ public interface CarbonSender extends Closeable {
         TcpMessenger messenger = new TcpMessenger(SocketChannel.open(socketAddress));
         return new CarbonSender() {
             @Override
-            public void counter(final String name, final long value) throws IOException {
+            public void gauge(final String name, final long value) throws IOException {
                 messenger.message(format("%s %s %s", name, value, Seconds.sinceEpoch(clock.now())));
             }
 
